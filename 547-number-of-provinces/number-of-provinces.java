c07820c4e5ef433ace690260e905baf0466dce7c
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-03-27 18:10:36 +0530

    Time: 3 ms (29.16%) | Memory: 47.1 MB (81.95%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,166 @@
+// Note : Basic DFS and BFS approach left (just find the number of connected components !!!)
+
+
+
+// TC: O(n^2 * α(n)) → we check all pairs + DSU operations are almost constant
+// SC: O(n) → parent[] and rank[] arrays
+
+class DisjointSet{
+
+  int n;
+  int rank[];
+  int parent[];
+
+   DisjointSet(int n)
+   {
+    this.n = n;
+    this.rank = new int[n];
+    this.parent = new int[n];
+
+    for(int i=0;i<n;i++)
+    {
+        rank[i] = 0;
+        parent[i] = i;
+    }
+   }
+
+   int find(int v)
+   {
+       if(v == parent[v])
+        return v;
+
+        return  parent[v] = find(parent[v]);
+   }
+
+   void unionByRank(int v1,int v2)
+   {
+       int leader1 = find(v1);
+       int leader2 = find(v2);
+
+       if(rank[leader1] > rank[leader2])
+       {
+           parent[leader2] = leader1;
+       }
+       else if(rank[leader1] < rank[leader2])
+       {
+           parent[leader1] = leader2;
+       }
+       else
+       {
+            parent[leader2] = leader1;
+            rank[leader1]++;
+       }
+   }
+}
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+
+          int n = isConnected.length;
+
+          DisjointSet ds = new DisjointSet(n);
+
+          int countPv = n; // consider in the beginning all cities are separated --> n provinces  !!!
+
+          for(int u=0;u<n;u++)
+          {
+             for(int v=0;v<n;v++)
+             {  
+                  // if:
+                // 1. u and v are different
+                // 2. they are directly connected
+                // 3. they are not already in same group
+                if( u != v && isConnected[u][v] == 1 && ds.find(u) != ds.find(v))
+                {
+                    ds.unionByRank(u,v);
+                    countPv--;  // since two groups merged → total provinces reduce by 1
+                }
+             }
+          }
+
+
+          return countPv;
+
+
+    }
+}
+
+
+// class DisjointSet{
+
+//   int n;
+//   int rank[];
+//   int parent[];
+
+//    DisjointSet(int n)
+//    {
+//     this.n = n;
+//     this.rank = new int[n];
+//     this.parent = new int[n];
+
+//     for(int i=0;i<n;i++)
+//     {
+//         rank[i] = 0;
+//         parent[i] = i;
+//     }
+//    }
+
+//    int find(int v)
+//    {
+//        if(v == parent[v])
+//         return v;
+
+//         return  parent[v] = find(parent[v]);
+//    }
+
+//    void unionByRank(int v1,int v2)
+//    {
+//        int leader1 = find(v1);
+//        int leader2 = find(v2);
+
+//        if(rank[leader1] > rank[leader2])
+//        {
+//            parent[leader2] = leader1;
+//        }
+//        else if(rank[leader1] < rank[leader2])
+//        {
+//            parent[leader1] = leader2;
+//        }
+//        else
+//        {
+//             parent[leader2] = leader1;
+//             rank[leader1]++;
+//        }
+//    }
+// }
+
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+
+//           int n = isConnected.length;
+
+//           DisjointSet ds = new DisjointSet(n);
+
+
+//           for(int u=0;u<n;u++)
+//           {
+//              for(int v=0;v<n;v++)
+//              {  
+//                 if( u != v && isConnected[u][v] == 1 && ds.find(u) != ds.find(v))
+//                     ds.unionByRank(u,v);
+//              }
+//           }
+
+//           int countPv = 0;
+
+//           for(int i=0;i<n;i++)
+        //    {
+        //       if(ds.parent[i] == i)
+        //       countPv++;
+        //    }
+
+//           return countPv;
+
+
+//     }
+// }
