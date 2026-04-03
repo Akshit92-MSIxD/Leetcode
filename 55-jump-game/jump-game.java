8ccba2195e9473cccc6f00befa297772e87243c6
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-03 17:10:46 +0530

    Time: 322 ms (7.08%) | Memory: 48.1 MB (31.64%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    int dfs(int[] nums,int start, int end, int[] dp)
+    {
+         if(dp[start] != -1)
+         return dp[start];
+         
+         if(start == end)
+         return 1;
+
+         int jumpSize = nums[start];
+
+         int ans = 0;
+
+         for(int jmp=1;jmp<=jumpSize;jmp++)
+         {
+            ans = dfs(nums,start+jmp,end,dp);
+
+            if(ans == 1)
+            return dp[start] = 1;
+         }
+
+         return dp[start] = ans;
+
+    }
+
+    public boolean canJump(int[] nums) {
+         
+         int n = nums.length;
+
+         int[] dp = new int[n];
+
+         for(int i=0;i<n;i++)
+           dp[i] = -1;
+         
+         int ans = dfs(nums,0,n-1,dp);
+
+         if(ans == 1)
+         return true;
+
+         return false;
+    }
+}
