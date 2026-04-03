ec97dd929c19e03a6ebc8e12bff4894671f2d4bb
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-03 17:26:56 +0530

    Time: 156 ms (8.51%) | Memory: 48.3 MB (14.73%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,43 +1,78 @@
 class Solution {
-    
-    int dfs(int[] nums,int start, int end, int[] dp)
-    {
-         if(dp[start] != -1)
-         return dp[start];
-         
-         if(start == end)
-         return 1;
-
-         int jumpSize = nums[start];
-
-         int ans = 0;
-
-         for(int jmp=1;jmp<=jumpSize;jmp++)
-         {
-            ans = dfs(nums,start+jmp,end,dp);
-
-            if(ans == 1)
-            return dp[start] = 1;
-         }
-
-         return dp[start] = ans;
-
-    }
 
     public boolean canJump(int[] nums) {
          
          int n = nums.length;
 
-         int[] dp = new int[n];
-
-         for(int i=0;i<n;i++)
-           dp[i] = -1;
+         boolean[] dp = new boolean[n];
          
-         int ans = dfs(nums,0,n-1,dp);
+         dp[n-1] = true;
 
-         if(ans == 1)
-         return true;
+         for(int i=n-2;i>=0;i--)
+         {
+            int jumpSize = nums[i];
 
-         return false;
+            for(int jmp=1;jmp<=jumpSize && (i+jmp)<n;jmp++)
+            {
+                dp[i] = dp[i+jmp];
+
+                if(dp[i] == true)
+                break;
+            }
+         }
+
+         return dp[0];
+
+
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+    
+//     int dfs(int[] nums,int start, int end, int[] dp)
+//     {
+//          if(dp[start] != -1)
+//          return dp[start];
+         
+//          if(start == end)
+//          return 1;
+
+//          int jumpSize = nums[start];
+
+//          int ans = 0;
+
+//          for(int jmp=1;jmp<=jumpSize;jmp++)
+//          {
+//             ans = dfs(nums,start+jmp,end,dp);
+
+//             if(ans == 1)
+//             return dp[start] = 1;
+//          }
+
+//          return dp[start] = ans;
+
+//     }
+
+//     public boolean canJump(int[] nums) {
+         
+//          int n = nums.length;
+
+//          int[] dp = new int[n];
+
+//          for(int i=0;i<n;i++)
+//            dp[i] = -1;
+         
+//          int ans = dfs(nums,0,n-1,dp);
+
+//          if(ans == 1)
+//          return true;
+
+//          return false;
+//     }
+// }
