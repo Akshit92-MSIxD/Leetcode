a2661fb9c57e234fe097afcf3356486c0a491433
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-04 17:29:46 +0530

    Time: 4 ms (44.13%) | Memory: 52.2 MB (70.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,180 +1,186 @@
+// DSU + Array approach left !!!
+// Please read both Approach 1 and Approach 2(DSU + HashMap) below !!!
+
+
 // Approach 1 : Using Normal DFS
 // Info : You can assume this problem is based on graphs!!!
 // This problem is related to Finding Connected Component in Graph !!!
 // Time complexity: O(r∗c)
 // Space complexity: O(r∗c)
 
-// class Solution {
+class Solution {
 
-//     void dfs(char[][] grid,int cr, int cc, boolean[][] vis)
-//     {
-//          if(cr < 0 || cr >= grid.length || cc < 0 || cc >= grid[cr].length || grid[cr][cc] == '0' || vis[cr][cc])
-//          return;
+    void dfs(char[][] grid,int cr, int cc, boolean[][] vis)
+    {
+         if(cr < 0 || cr >= grid.length || cc < 0 || cc >= grid[cr].length || grid[cr][cc] == '0' || vis[cr][cc])
+         return;
 
-//         vis[cr][cc] = true;
+        vis[cr][cc] = true;
           
-//         dfs(grid,cr,cc-1,vis); // going to left neighbour vertex(element)
-//         dfs(grid,cr,cc+1,vis); // going to right neighbour vertex(element)
-//         dfs(grid,cr-1,cc,vis); // going to top neighbour vertex(element)
-//         dfs(grid,cr+1,cc,vis); // going to bottom neighbour vertex(element)
+        dfs(grid,cr,cc-1,vis); // going to left neighbour vertex(element)
+        dfs(grid,cr,cc+1,vis); // going to right neighbour vertex(element)
+        dfs(grid,cr-1,cc,vis); // going to top neighbour vertex(element)
+        dfs(grid,cr+1,cc,vis); // going to bottom neighbour vertex(element)
 
-//         return;
-//     }
+        return;
+    }
 
-//     public int numIslands(char[][] grid) {
+    public int numIslands(char[][] grid) {
          
-//          boolean[][] vis = new boolean[grid.length][grid[0].length];
+         boolean[][] vis = new boolean[grid.length][grid[0].length];
 
-//          int ans = 0;
+         int ans = 0;
         
-//          // consider each block of grid array as vertex of a graph and each vertex is connected horizontally and vertically to
-//          //   neighbour vertices with invisible edges in grid array !!!
+         // consider each block of grid array as vertex of a graph and each vertex is connected horizontally and vertically to
+         //   neighbour vertices with invisible edges in grid array !!!
 
 
-//          // Traversing each vertex from left to right and top to bottom !!!
-//          for(int i=0;i<grid.length;i++)
-//          {
-//             for(int j=0;j<grid[i].length;j++)
-//             {
-//                 if(grid[i][j] == '1' && !vis[i][j])
-//                 {
-//                     dfs(grid,i,j,vis);
-//                     ans++;
-//                 }
-//             }
-//          }
+         // Traversing each vertex from left to right and top to bottom !!!
+         for(int i=0;i<grid.length;i++)
+         {
+            for(int j=0;j<grid[i].length;j++)
+            {
+                if(grid[i][j] == '1' && !vis[i][j])
+                {
+                    dfs(grid,i,j,vis);
+                    ans++;
+                }
+            }
+         }
 
-//          return ans;
+         return ans;
 
           
-//     }
-// }
-
+    }
+}
 
 
 
 /*--------------------------------------------------------------------------------------------*/
 
 
+// Approach 2 : Using DSU + HashMap(Written By Me :) (!!! Space optimized algo)
+// TC : O(n*m)
+// SC : O(n*m) (Only in worst case when there is no(water) in the matrix)
 
-class DisjointSet{
+// class DisjointSet{
 
- HashMap<Integer,Integer> parent;
- HashMap<Integer,Integer> rank;
+//  HashMap<Integer,Integer> parent;
+//  HashMap<Integer,Integer> rank;
 
- DisjointSet(){
-     parent = new HashMap<>();
-     rank = new HashMap<>();
- }
+//  DisjointSet(){
+//      parent = new HashMap<>();
+//      rank = new HashMap<>();
+//  }
 
- int find(int node)
- {
-    if(node == parent.get(node))
-     return node;
+//  int find(int node)
+//  {
+//     if(node == parent.get(node))
+//      return node;
 
-     int leader = find(parent.get(node));
-     parent.put(node,leader);
-     return leader;
- }
+//      int leader = find(parent.get(node));
+//      parent.put(node,leader);
+//      return leader;
+//  }
 
- void unionByRank(int node1, int node2)
- {
-    int leader1 = find(node1);
-    int leader2 = find(node2);
+//  void unionByRank(int node1, int node2)
+//  {
+//     int leader1 = find(node1);
+//     int leader2 = find(node2);
 
-    if(leader1 == leader2)return;
+//     if(leader1 == leader2)return;
 
-    int r1 = rank.get(leader1);
-    int r2 = rank.get(leader2);
+//     int r1 = rank.get(leader1);
+//     int r2 = rank.get(leader2);
 
-    if(r1 > r2)
-    {
-       parent.put(leader2,leader1);
-    }
-    else if(r1 < r2)
-    {
-       parent.put(leader1,leader2);
-    }
-    else
-    {
-       parent.put(leader2,leader1);
-       rank.put(leader1,rank.get(leader1) + 1);
-    }
- }
+//     if(r1 > r2)
+//     {
+//        parent.put(leader2,leader1);
+//     }
+//     else if(r1 < r2)
+//     {
+//        parent.put(leader1,leader2);
+//     }
+//     else
+//     {
+//        parent.put(leader2,leader1);
+//        rank.put(leader1,rank.get(leader1) + 1);
+//     }
+//  }
 
-}
-
-
-class Solution {
+// }
 
 
-    public int numIslands(char[][] grid) {
+// class Solution {
 
-        DisjointSet ds = new DisjointSet();
+
+//     public int numIslands(char[][] grid) {
+
+//         DisjointSet ds = new DisjointSet();
          
-        int rows = grid.length;
-        int cols = grid[0].length;
+//         int rows = grid.length;
+//         int cols = grid[0].length;
        
-        // traversing to put only valid island nodes in the ds
-        for(int r=0;r<rows;r++)
-        {
-            for(int c=0;c<cols;c++)
-            {
-                if(grid[r][c] == '1')
-                {
-                int node = r*cols + c;
-                ds.parent.put(node,node);
-                ds.rank.put(node,1);
-                }
-            }
-        }
+//         // traversing to put only valid island nodes in the ds
+//         for(int r=0;r<rows;r++)
+//         {
+//             for(int c=0;c<cols;c++)
+//             {
+//                 if(grid[r][c] == '1')
+//                 {
+//                 int node = r*cols + c;
+//                 ds.parent.put(node,node);
+//                 ds.rank.put(node,1);
+//                 }
+//             }
+//         }
 
 
-        int[][] directions = {{0,1},{1,0}}; // right and bottom
+//         int[][] directions = {{0,1},{1,0}}; // right and bottom
 
 
-        // traversing again to join/union island nodes
+//         // traversing again to join/union island nodes
 
-        for(int r=0;r<rows;r++)
-        {
-            for(int c=0;c<cols;c++)
-            {
-                if(grid[r][c] == '1')
-                {
-                    int curr_node = r*cols + c;
+//         for(int r=0;r<rows;r++)
+//         {
+//             for(int c=0;c<cols;c++)
+//             {
+//                 if(grid[r][c] == '1')
+//                 {
+//                     int curr_node = r*cols + c;
 
-                    // explore its neigbhours only right and bottom !!!
+//                     // explore its neigbhours only right and bottom !!!
 
-                    for(int[] dir : directions)
-                    {
-                        int nbr_r = r + dir[0];
-                        int nbr_c = c + dir[1];
+//                     for(int[] dir : directions)
+//                     {
+//                         int nbr_r = r + dir[0];
+//                         int nbr_c = c + dir[1];
 
-                        if(nbr_r<rows && nbr_c<cols && grid[nbr_r][nbr_c] == '1')
-                        {
-                          int nbr_node = nbr_r*cols + nbr_c;
-                          ds.unionByRank(curr_node,nbr_node);
-                        }
-                    }
-                }
-            }
-        }
+//                         if(nbr_r<rows && nbr_c<cols && grid[nbr_r][nbr_c] == '1')
+//                         {
+//                           int nbr_node = nbr_r*cols + nbr_c;
+//                           ds.unionByRank(curr_node,nbr_node);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
 
-        int count_islands = 0;
+//         int count_islands = 0;
 
-        for(int node : ds.parent.keySet())
-        {
-            if(ds.parent.get(node) == node)
-            count_islands++;
-        }
+//         for(int node : ds.parent.keySet())
+//         {
+//             if(ds.parent.get(node) == node)
+//             count_islands++;
+//         }
 
-        return count_islands;
+//         return count_islands;
 
 
 
           
-    }
-}
+//     }
+// }
 
 
 
