458080e3fbafee4529b867ee9c9f4358e3d94c98
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-04 18:31:45 +0530

    Time: 1 ms (41.79%) | Memory: 46.7 MB (70.75%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,64 @@
+
+// Approach : Using BFS
+
+class Node{
+   
+   int r , c;
+
+   Node(int r, int c)
+   {
+     this.r = r;
+     this.c = c;
+   }
+
+}
+class Solution {
+
+    void bfs(int[][] image, int sr, int sc, int color)
+    {
+        int rows = image.length;
+        int cols = image[0].length;
+
+        int org_color = image[sr][sc];
+
+        if(org_color == color) return;
+
+
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(sr,sc));
+
+
+        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}}; // top , bottom , left , right
+
+        while(!q.isEmpty())
+        {
+            Node top = q.poll();
+
+            int curr_r = top.r;
+            int curr_c = top.c;
+
+            image[curr_r][curr_c] = color;
+
+            // explore the neighbours
+
+            for(int[] dir : directions)
+            {
+                int nbr_r = curr_r + dir[0];
+                int nbr_c = curr_c + dir[1];
+
+                if(nbr_r>=0 && nbr_r<rows && nbr_c>=0 && nbr_c<cols && image[nbr_r][nbr_c] == org_color)
+                    q.add(new Node(nbr_r,nbr_c));
+            }
+        }
+    }
+
+
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+          bfs(image,sr,sc,color);
+
+          return image;
+
+    }
+}
