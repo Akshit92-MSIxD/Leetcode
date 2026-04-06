3dff46ddcfe6989f61bdf9640275e47af0db7286
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-06 18:24:58 +0530

    Time: 1075 ms (0.00%) | Memory: 146.9 MB (100.00%) - LeetSync

---
+++ 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> findGoodIntegers(int n) {
+
+        List<Integer> ans = new ArrayList<>();
+         
+        Map<Integer,Integer> mp = new TreeMap<>();
+
+        for(int a = 1; a*a*a < n;a++)
+        {
+            int sum = 0;
+            for(int b=a;b*b*b < n;b++)
+            {
+               sum = sum + a*a*a + b*b*b;
+               if(mp.containsKey(sum) == false && sum <= n)
+                mp.put(sum,1);
+               else if(sum<=n)
+                mp.put(sum,mp.get(sum)+1);
+
+                sum = 0;
+            }
+        }
+
+
+        for(int num : mp.keySet())
+        {
+            if(num <= n && mp.get(num) > 1)
+            ans.add(num);
+        }
+
+        return ans;
+
+        
+    }
+}
