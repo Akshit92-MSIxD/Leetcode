5114af04eda1b16895e61aa5d17bab1491d66b5a
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-07 20:12:07 +0530

    Time: 36 ms (19.98%) | Memory: 169.9 MB (5.01%) - LeetSync

--- 4149-minimum-increase-to-maximize-special-indices/minimum-increase-to-maximize-special-indices.java
+++ 4149-minimum-increase-to-maximize-special-indices/minimum-increase-to-maximize-special-indices.java
@@ -107,81 +107,110 @@
 // }
 
 
-class Pair{
+// class Pair{
  
- int maxInd = 0;
- long minOprs = 0;
+//  int maxInd = 0;
+//  long minOprs = 0;
  
-  Pair(int minInd,long minOprs)
-  {
-    this.maxInd = maxInd;
-    this.minOprs = minOprs;
-  }
+//   Pair(int minInd,long minOprs)
+//   {
+//     this.maxInd = maxInd;
+//     this.minOprs = minOprs;
+//   }
 
-}
-class Solution {
+// }
+// class Solution {
 
-    public long minIncrease(int[] nums) {
+//     public long minIncrease(int[] nums) {
          
-         int n = nums.length;
+//          int n = nums.length;
         
-         int maxSpecialindices = -1;
+//          int maxSpecialindices = -1;
 
-         long[] cost = new long[n];
-         cost[0] = 0;
-         cost[n-1] = 0;
+//          long[] cost = new long[n];
+//          cost[0] = 0;
+//          cost[n-1] = 0;
 
-         for(int i=1;i<n-1;i++)
-         {   
-            long large = Math.max(nums[i-1],nums[i+1]);
-            cost[i] = Math.max(0,large - nums[i] + 1);
-         }
+//          for(int i=1;i<n-1;i++)
+//          {   
+//             long large = Math.max(nums[i-1],nums[i+1]);
+//             cost[i] = Math.max(0,large - nums[i] + 1);
+//          }
          
-         Pair[] dp = new Pair[n+1];
+//          Pair[] dp = new Pair[n+1];
 
-         for(int i=0;i<=n;i++)
-         dp[i] = new Pair(0,0);
+//          for(int i=0;i<=n;i++)
+//          dp[i] = new Pair(0,0);
 
 
-         for(int i = n-2;i>=1;i--)
-         {
-            Pair take = dp[i+2];
-            take.maxInd += 1;
-            take.minOprs += cost[i];
+//          for(int i = n-2;i>=1;i--)
+//          {
+//             Pair take = dp[i+2];
+//             take.maxInd += 1;
+//             take.minOprs += cost[i];
 
-            Pair notTake = dp[i+1];
+//             Pair notTake = dp[i+1];
 
-            if(take.maxInd > notTake.maxInd)
-            {
-                dp[i].maxInd = take.maxInd;
-                dp[i].minOprs = take.minOprs;
-            }
-            else if(take.maxInd == notTake.maxInd)
-            {
-                if(take.minOprs < notTake.minOprs)
-                {
-                 dp[i].maxInd = take.maxInd;
-                 dp[i].minOprs = take.minOprs;
-                }
-                else
-                {
-                 dp[i].maxInd = notTake.maxInd;
-                 dp[i].minOprs = notTake.minOprs;
-                }
-            }
-            else
-            {
-                dp[i].maxInd = notTake.maxInd;
-                dp[i].minOprs = notTake.minOprs;
-            }
+//             if(take.maxInd > notTake.maxInd)
+//             {
+//                 dp[i].maxInd = take.maxInd;
+//                 dp[i].minOprs = take.minOprs;
+//             }
+//             else if(take.maxInd == notTake.maxInd)
+//             {
+//                 if(take.minOprs < notTake.minOprs)
+//                 {
+//                  dp[i].maxInd = take.maxInd;
+//                  dp[i].minOprs = take.minOprs;
+//                 }
+//                 else
+//                 {
+//                  dp[i].maxInd = notTake.maxInd;
+//                  dp[i].minOprs = notTake.minOprs;
+//                 }
+//             }
+//             else
+//             {
+//                 dp[i].maxInd = notTake.maxInd;
+//                 dp[i].minOprs = notTake.minOprs;
+//             }
 
             
-         }
+//          }
 
-         return dp[1].minOprs;
+//          return dp[1].minOprs;
 
 
     
 
+//     }
+// }
+
+
+class Solution {
+    public long minIncrease(int[] nums) {
+        int n = nums.length;
+
+        long[] cost = new long[n];
+        for(int i = 1; i < n-1; i++) {
+            long large = Math.max(nums[i-1], nums[i+1]);
+            cost[i] = Math.max(0, large - nums[i] + 1);
+        }
+
+        // dp[i] = {maxSpecial, minOps}
+        long[][] dp = new long[n+1][2];
+
+        for(int i = n-2; i >= 1; i--) {
+            long[] take    = {dp[i+2][0] + 1, dp[i+2][1] + cost[i]};
+            long[] notTake = {dp[i+1][0],     dp[i+1][1]};
+
+            // pick whichever gives more special, break ties by min cost
+            if(take[0] > notTake[0] || (take[0] == notTake[0] && take[1] < notTake[1]))
+                dp[i] = take;
+            else
+                dp[i] = notTake;
+        }
+
+        return dp[1][1];
     }
 }
