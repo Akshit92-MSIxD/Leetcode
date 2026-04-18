f6e1c839c6e63987093c8dbcb0ed507f3b54d6c2
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-18 18:41:26 +0530

    Time: 398 ms (14.39%) | Memory: 273.4 MB (34.31%) - LeetSync

--- 4265-longest-balanced-substring-after-one-swap/longest-balanced-substring-after-one-swap.java
+++ 4265-longest-balanced-substring-after-one-swap/longest-balanced-substring-after-one-swap.java
@@ -95,115 +95,248 @@
 
 
 
+/*-------------------------------------------------------------------------------------------------------*/
+
+
+
+// class Solution {
+//     public int longestBalanced(String s) {
+        
+//         int n = s.length();
+//         HashMap<Character,Integer> freq = new HashMap<>();
+
+//         for(char ch : s.toCharArray())
+//         {
+//             if(!freq.containsKey(ch))
+//              freq.put(ch,1);
+//              else
+//              freq.put(ch,freq.get(ch)+1);
+//         }
+
+
+//         int[] prefix = new int[n];
+
+//         HashMap<Integer,List<Integer>> occ_index = new HashMap<>();
+        
+//         occ_index.put(0,new ArrayList<>());
+//         occ_index.get(0).add(-1);
+
+//         int maxLen = 0;
+
+//         for(int i=0;i<n;i++)
+//                 {   
+//                     char ch = s.charAt(i);
+
+//                 if(i==0)
+//                 {
+//                     prefix[0] = (ch == '0') ? -1 : 1;
+//                     occ_index.put(prefix[0],new ArrayList<>());
+//                     occ_index.get(prefix[0]).add(0);
+//                     continue;
+//                 }
+
+//                 prefix[i] = prefix[i-1] + ((ch == '0') ? -1 : 1);
+
+//                 if(prefix[i] == 0)
+//                 {
+//                     maxLen = Math.max(maxLen,i+1);
+//                     occ_index.get(0).add(i);
+//                     continue;
+//                 }
+
+//                 if(occ_index.containsKey(prefix[i]-2))
+//                 {
+//                     for(int pi : occ_index.get(prefix[i]-2))
+//                     {
+//                         int len = i - pi;
+                        
+//                         if(len > maxLen)
+//                         {
+//                             int zeroes = (len/2) - 1;
+
+//                             if(freq.containsKey('0') && (freq.get('0') - zeroes) > 0)
+//                             {
+//                             maxLen = Math.max(maxLen,len);
+//                             break;
+//                             }
+
+//                         }
+//                         else
+//                         {
+//                             break;
+//                         }
+//                     }
+//                 }
+
+//                 if(occ_index.containsKey(prefix[i]+2))
+//                 {
+//                     for(int pi : occ_index.get(prefix[i]+2))
+//                     {
+//                         int len = i - pi;
+                        
+//                         if(len > maxLen)
+//                         {
+//                             int ones = (len/2) - 1;
+
+//                             if(freq.containsKey('1') && (freq.get('1') - ones) > 0)
+//                             {
+//                             maxLen = Math.max(maxLen,len);
+//                             break;
+//                             }
+
+//                         }
+//                         else
+//                         {
+//                             break;
+//                         }
+//                     }
+//                 }
+
+
+//               if(occ_index.containsKey(prefix[i]))
+//               {
+//                  int foi = occ_index.get(prefix[i]).get(0);  // foi -> first occurence index of prefix[i]
+//                  maxLen = Math.max(maxLen,i-foi);
+//                  occ_index.get(prefix[i]).add(i);
+//               }
+//               else
+//               {
+//                 occ_index.put(prefix[i],new ArrayList<>());
+//                 occ_index.get(prefix[i]).add(i);
+//               }
+
+//            }
+
+//           return maxLen;
+           
+//         }
+//     }
+
+
+
+/*--------------------------------------------------------------------------------------------------------*/
+
+
+
 class Solution {
-    public int longestBalanced(String s) {
-        
-        int n = s.length();
-        HashMap<Character,Integer> freq = new HashMap<>();
 
-        for(char ch : s.toCharArray())
-        {
-            if(!freq.containsKey(ch))
-             freq.put(ch,1);
-             else
-             freq.put(ch,freq.get(ch)+1);
-        }
+    int getLongestBalancedSubstringLength(String s, int zeroes, int ones)
+    {
+           int n = s.length();
+           int[] prefix = new int[n];
+           HashMap<Integer,Integer> first_occ_index = new HashMap<>();
+           first_occ_index.put(0,-1);
 
+           int maxLen = 0;
 
-        int[] prefix = new int[n];
+           for(int i=0;i<n;i++)
+           {
+               char ch = s.charAt(i);
+               if(i==0)
+               {
+                  prefix[i] = ((ch == '1') ? 1 : -1);
 
-        HashMap<Integer,List<Integer>> occ_index = new HashMap<>();
-        
-        occ_index.put(0,new ArrayList<>());
-        occ_index.get(0).add(-1);
+                  if(prefix[i] == 1)
+                  ones--;
+                  else
+                  zeroes--;
 
-        int maxLen = 0;
+                  first_occ_index.put(prefix[i],i);
+                  continue;
+               }
 
-        for(int i=0;i<n;i++)
-                {   
-                    char ch = s.charAt(i);
+               prefix[i] = prefix[i-1];
 
-                if(i==0)
-                {
-                    prefix[0] = (ch == '0') ? -1 : 1;
-                    occ_index.put(prefix[0],new ArrayList<>());
-                    occ_index.get(prefix[0]).add(0);
-                    continue;
-                }
+               if(ch == '1')
+               {
+                  prefix[i] += 1;
+                  ones--;
+               }
+               else
+               {
+                  prefix[i] += -1;
+                  zeroes--;
+               }
 
-                prefix[i] = prefix[i-1] + ((ch == '0') ? -1 : 1);
+               if(prefix[i] == 0)
+               {
+                  maxLen = Math.max(maxLen,i+1);
+                  continue;
+               }
 
-                if(prefix[i] == 0)
-                {
-                    maxLen = Math.max(maxLen,i+1);
-                    occ_index.get(0).add(i);
-                    continue;
-                }
+               if(first_occ_index.containsKey(prefix[i]-2))
+               {
+                    int foi = first_occ_index.get(prefix[i]-2);
+                    int len = i - foi;
 
-                if(occ_index.containsKey(prefix[i]-2))
-                {
-                    for(int pi : occ_index.get(prefix[i]-2))
-                    {
-                        int len = i - pi;
-                        
-                        if(len > maxLen)
-                        {
-                            int zeroes = (len/2) - 1;
+                    if(zeroes > 0)
+                    maxLen = Math.max(maxLen,len);
+               }
 
-                            if(freq.containsKey('0') && (freq.get('0') - zeroes) > 0)
-                            {
-                            maxLen = Math.max(maxLen,len);
-                            break;
-                            }
+               if(first_occ_index.containsKey(prefix[i]+2))
+               {
+                   int foi = first_occ_index.get(prefix[i]+2);
+                   int len = i - foi;
 
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-
-                if(occ_index.containsKey(prefix[i]+2))
-                {
-                    for(int pi : occ_index.get(prefix[i]+2))
-                    {
-                        int len = i - pi;
-                        
-                        if(len > maxLen)
-                        {
-                            int ones = (len/2) - 1;
-
-                            if(freq.containsKey('1') && (freq.get('1') - ones) > 0)
-                            {
-                            maxLen = Math.max(maxLen,len);
-                            break;
-                            }
-
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
+                   if(ones > 0)
+                   maxLen = Math.max(maxLen,len);
+               }
 
 
-              if(occ_index.containsKey(prefix[i]))
-              {
-                 int foi = occ_index.get(prefix[i]).get(0);  // foi -> first occurence index of prefix[i]
-                 maxLen = Math.max(maxLen,i-foi);
-                 occ_index.get(prefix[i]).add(i);
-              }
-              else
-              {
-                occ_index.put(prefix[i],new ArrayList<>());
-                occ_index.get(prefix[i]).add(i);
-              }
+               if(first_occ_index.containsKey(prefix[i]))
+               {
+                   int foi = first_occ_index.get(prefix[i]);
+                   int len = i - foi;
+                   maxLen = Math.max(maxLen,len);
+               }
+               else
+               {
+                  first_occ_index.put(prefix[i],i);
+               }
 
            }
 
-          return maxLen;
+           return maxLen;
+    }
+
+    String reverse(String s)
+    {
+        StringBuilder rev_s = new StringBuilder();
+
+        for(int i=s.length()-1;i>=0;i--)
+        {
+           char ch = s.charAt(i);
+           rev_s.append(ch);
+        }
+
+        return rev_s.toString();
+    }
+
+
+    public int longestBalanced(String s) {
+         
+            int n = s.length();
+
+           int ones = 0;
+           int zeroes = 0;
            
+           for(int i=0;i<n;i++)
+           {
+              char ch = s.charAt(i);
+                
+                if(ch == '1')
+                ones++;
+                else
+                zeroes++;
+           }
+ 
+          int maxLen = getLongestBalancedSubstringLength(s,zeroes,ones);
+
+          String rev_s = reverse(s);
+
+          maxLen = Math.max(maxLen,getLongestBalancedSubstringLength(rev_s,zeroes,ones));
+
+          return maxLen;
+
         }
     }
