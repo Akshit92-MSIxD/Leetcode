23dc4d09d34b8a39cea2daca5ef7b7c67338c56f
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-19 12:33:44 +0530

    Time: 52 ms (94.73%) | Memory: 89.1 MB (99.96%) - LeetSync

---
+++ 4283-multi-source-flood-fill/multi-source-flood-fill.java
@@ -0,0 +1,195 @@
+
+// class Node
+//     {
+//         int r;
+//         int c;
+//         int lvl;
+
+//         Node(int r, int c, int lvl)
+//         {
+//            this.r = r;
+//            this.c = c;
+//            this.lvl = lvl;
+//         }
+//     }
+
+// class Solution {
+//     public int[][] colorGrid(int n, int m, int[][] sources) {
+
+//          int[][] grid = new int[n][m];
+         
+        
+
+//         for(int i=0;i<sources.length;i++)
+//             { 
+//                 int r = sources[i][0];
+//                 int c = sources[i][1];
+//                 int val = sources[i][2];
+
+//                 grid[r][c] = val;
+
+//             }
+
+//             int rows = grid.length;
+//             int cols = grid[0].length;
+        
+//             HashMap<Integer,Integer> vis_lvl = new HashMap<>();
+        
+//            Queue<Node> q = new ArrayDeque<>();
+
+//         for(int i=0;i<rows;i++)
+//             {
+//                 for(int j=0;j<cols;j++)
+//                     {
+//                         if(grid[i][j] != 0)
+//                         {
+//                         q.add(new Node(i,j,0));
+//                         int node = i*cols + j;
+//                         vis_lvl.put(node,0);
+//                         }
+//                     }
+//             }
+
+//         int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+
+//         while(!q.isEmpty())
+//             {
+//                 Node top = q.poll();
+//                 int cr = top.r;
+//                 int cc = top.c;
+//                 int clvl = top.lvl;
+
+//                 int curr_node = cr*cols + cc;
+
+//                  for(int[] dir : directions)
+//                      {
+//                          int nbr_r = cr + dir[0];
+//                          int nbr_c = cc + dir[1];
+
+//                          if(nbr_r >=0 && nbr_r<rows && nbr_c>=0 && nbr_c<cols)
+//                          {
+//                              int nbr_node = nbr_r*cols + nbr_c;
+
+//                              if(!vis_lvl.containsKey(nbr_node))
+//                              {
+//                                  grid[nbr_r][nbr_c] = grid[cr][cc];
+//                                  q.add(new Node(nbr_r,nbr_c,clvl+1));
+//                                  vis_lvl.put(nbr_node,clvl+1);
+//                              }
+//                              else
+//                              {
+//                                  if(clvl + 1 == vis_lvl.get(nbr_node) && grid[cr][cc] > grid[nbr_r][nbr_c])
+//                                  {
+//                                      grid[nbr_r][nbr_c] = grid[cr][cc];
+//                                  }
+//                              }
+//                          }
+//                      }
+
+                
+//             }
+
+//          return grid;
+           
+//     }
+// }
+
+
+class Node
+    {
+        int r;
+        int c;
+        int lvl;
+
+        Node(int r, int c, int lvl)
+        {
+           this.r = r;
+           this.c = c;
+           this.lvl = lvl;
+        }
+    }
+
+class Solution {
+    public int[][] colorGrid(int n, int m, int[][] sources) {
+
+         int[][] grid = new int[n][m];
+         
+        
+
+        for(int i=0;i<sources.length;i++)
+            { 
+                int r = sources[i][0];
+                int c = sources[i][1];
+                int val = sources[i][2];
+
+                grid[r][c] = val;
+
+            }
+
+            int rows = grid.length;
+            int cols = grid[0].length;
+        
+            int[][] vis_lvl = new int[rows][cols];
+
+            for(int i=0;i<rows;i++)
+            {
+                for(int j=0;j<cols;j++)
+                {
+                    vis_lvl[i][j] = -1;
+                }
+            }
+        
+           Queue<Node> q = new ArrayDeque<>();
+
+        for(int i=0;i<rows;i++)
+            {
+                for(int j=0;j<cols;j++)
+                    {
+                        if(grid[i][j] != 0)
+                        {
+                        q.add(new Node(i,j,0));
+                        vis_lvl[i][j] = 0;
+                        }
+                    }
+            }
+
+        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        while(!q.isEmpty())
+            {
+                Node top = q.poll();
+                int cr = top.r;
+                int cc = top.c;
+                int clvl = top.lvl;
+
+                int curr_node = cr*cols + cc;
+
+                 for(int[] dir : directions)
+                     {
+                         int nbr_r = cr + dir[0];
+                         int nbr_c = cc + dir[1];
+
+                         if(nbr_r >=0 && nbr_r<rows && nbr_c>=0 && nbr_c<cols)
+                         {
+
+                             if(vis_lvl[nbr_r][nbr_c] == -1)
+                             {
+                                 grid[nbr_r][nbr_c] = grid[cr][cc];
+                                 q.add(new Node(nbr_r,nbr_c,clvl+1));
+                                 vis_lvl[nbr_r][nbr_c] = clvl + 1;
+                             }
+                             else
+                             {
+                                 if(clvl + 1 == vis_lvl[nbr_r][nbr_c] && grid[cr][cc] > grid[nbr_r][nbr_c])
+                                     grid[nbr_r][nbr_c] = grid[cr][cc];
+                             }
+                         }
+                     }
+
+                
+            }
+
+         return grid;
+           
+    }
+}
