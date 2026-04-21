e9583f137fdc8937ab73ee31351becf122bde042
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-22 00:48:31 +0530

    Time: 4 ms (99.85%) | Memory: 64.8 MB (15.26%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,70 +1,132 @@
+// Please write proper comments and watch video solution for this problem !!!
+
+
+// class Solution {
+    
+//     boolean dfs(int[][] graph, int curr_node,int[] visState, boolean[] pathVis)
+//     {
+//           visState[curr_node] = 1;
+//           pathVis[curr_node] = true;
+
+//           // explore the neighbours
+
+//           boolean ans = true;
+
+//           for(int nbr_node : graph[curr_node])
+//           {
+//               if(visState[nbr_node] != 0)
+//               {
+//                   if(visState[nbr_node] == 2)
+//                   {
+//                     visState[curr_node] = 2;
+//                     ans = false;
+//                   }
+//                   else
+//                   {
+//                      if(pathVis[nbr_node])
+//                      {
+//                         visState[curr_node] = 2;
+//                         ans = false;
+//                      }
+//                   }
+//               }
+//               else
+//               {
+//                  if(dfs(graph,nbr_node,visState,pathVis) == false)
+//                  {
+//                     visState[curr_node] = 2;
+//                     ans = false;
+//                  }
+//               }
+//           }
+
+//           pathVis[curr_node] = false;
+
+//           return ans;
+//     }
+
+//     public List<Integer> eventualSafeNodes(int[][] graph) {
+          
+//           int n = graph.length;
+
+//           int[] visState = new int[n];
+
+//           for(int i=0;i<n;i++)
+//           visState[i] = 0;
+
+//           boolean[] pathVis = new boolean[n];
+
+
+//           for(int node=0;node<n;node++)
+//           {
+//                if(visState[node] == 0)
+//                 dfs(graph,node,visState,pathVis);
+//           }
+
+//           List<Integer> ans = new ArrayList<>();
+
+//           for(int node=0;node<n;node++)
+//           {
+//             if(visState[node] == 1)
+//             ans.add(node);
+//           }
+
+//           return ans;
+ 
+//     }
+// }
+
+
 class Solution {
     
-    boolean dfs(int[][] graph, int curr_node,int[] visState, boolean[] pathVis)
+    boolean dfs(int[][] graph, int curr_node,boolean[] vis, boolean[] pathVis)
     {
-          visState[curr_node] = 1;
+          vis[curr_node] = true;
           pathVis[curr_node] = true;
 
           // explore the neighbours
 
-          boolean ans = true;
-
           for(int nbr_node : graph[curr_node])
           {
-              if(visState[nbr_node] != 0)
+              if(vis[nbr_node])
               {
-                  if(visState[nbr_node] == 2)
-                  {
-                    visState[curr_node] = 2;
-                    ans = false;
-                  }
-                  else
-                  {
-                     if(pathVis[nbr_node])
-                     {
-                        visState[curr_node] = 2;
-                        ans = false;
-                     }
-                  }
+                if(pathVis[nbr_node])
+                    return true;
               }
               else
               {
-                 if(dfs(graph,nbr_node,visState,pathVis) == false)
-                 {
-                    visState[curr_node] = 2;
-                    ans = false;
-                 }
+                if(dfs(graph,nbr_node,vis,pathVis) == true)
+                return true;
               }
           }
 
+         
+
           pathVis[curr_node] = false;
 
-          return ans;
+          return false;
     }
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
           
           int n = graph.length;
 
-          int[] visState = new int[n];
-
-          for(int i=0;i<n;i++)
-          visState[i] = 0;
+          boolean[] vis = new boolean[n];
 
           boolean[] pathVis = new boolean[n];
 
 
           for(int node=0;node<n;node++)
           {
-               if(visState[node] == 0)
-                dfs(graph,node,visState,pathVis);
+               if(!vis[node])
+                dfs(graph,node,vis,pathVis);
           }
 
           List<Integer> ans = new ArrayList<>();
 
           for(int node=0;node<n;node++)
           {
-            if(visState[node] == 1)
+            if(pathVis[node] == false)
             ans.add(node);
           }
 
