98d9831b333a2f5e598307997b3f98530fa18cd4
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-04-29 17:33:22 +0530

    Time: 1 ms (99.55%) | Memory: 47.2 MB (75.11%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,25 +1,55 @@
 class Solution {
     public int jump(int[] nums) {
          
-          int n = nums.length;
-          
-          int[] dp = new int[n];
+         int n = nums.length;
 
-          for(int i=0;i<n;i++)
-           dp[i] = 100000;
+         int l = 0;
+         int r = 0;
+         int farthest = 0;
+         int jumps = 0;
 
-          dp[n-1] = 0;
-
-          for(int i=n-2;i>=0;i--)
+          while(r < n - 1)
           {
-              int jmpSize = nums[i];
-
-              for(int jmp=1;jmp<=jmpSize && (i+jmp)<n;jmp++)
-                 dp[i] = Math.min(dp[i],1 + dp[i+jmp]);
+             for(int i=l;i<=r;i++)
+             {
+                int maxJmp = nums[i];
+                farthest = Math.max(farthest,i+maxJmp);
+             }
+             l = r+1;
+             r = farthest;
+             jumps++;
           }
 
-          return dp[0];
-
+          return jumps;
 
     }
 }
+
+
+
+// class Solution {
+//     public int jump(int[] nums) {
+         
+//           int n = nums.length;
+          
+//           int[] dp = new int[n];
+
+//           for(int i=0;i<n;i++)
+//            dp[i] = 100000;
+
+//           dp[n-1] = 0;
+
+//           for(int i=n-2;i>=0;i--)
+//           {
+//               int jmpSize = nums[i];
+
+//               for(int jmp=1;jmp<=jmpSize && (i+jmp)<n;jmp++)
+//                  dp[i] = Math.min(dp[i],1 + dp[i+jmp]);
+//           }
+
+//           return dp[0];
+
+
+//     }
+// }
+
