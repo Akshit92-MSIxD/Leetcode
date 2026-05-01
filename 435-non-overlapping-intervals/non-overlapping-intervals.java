a7dc5646dbdbeb1d2fff983bc61c39e4856d85cc
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-01 12:26:21 +0530

    Time: 60 ms (8.87%) | Memory: 116 MB (8.95%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -43,7 +43,7 @@
 //           int s2 = in[i].start;
 //           int e2 = in[i].end;
 
-//           if(s2 >= s1 && s2 < e1)
+//           if(s2 < e1)
 //           {
 //               minRemovals++;
 
@@ -64,60 +64,38 @@
 // }
 
 
-class Interval{
-
-    int start;
-    int end;
-
-    Interval(int start, int end)
-    {
-        this.start = start;
-        this.end = end;
-    }
-}
-
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
          
      int n = intervals.length;
 
-     Interval[] in = new Interval[n];
-
-     for(int i=0;i<n;i++)
-     {
-        int start = intervals[i][0];
-        int end = intervals[i][1];
-
-        in[i] = new Interval(start,end);
-     }
-
-
-     Arrays.sort(in,(a,b)->{
-        if(a.start != b.start) return a.start - b.start;
-        return a.end - b.end;
+     Arrays.sort(intervals,(a,b)->{
+         if(a[0] != b[0]) return Integer.compare(a[0],b[0]);
+         return a[1] - b[1];
      });
 
+     int[] comp = intervals[0];
      int minRemovals = 0;
-
-      Interval comp = in[0]; 
 
       for(int i=1;i<n;i++)
       {
-          int s1 = comp.start;
-          int e1 = comp.end;
-          int s2 = in[i].start;
-          int e2 = in[i].end;
+          int s1 = comp[0];
+          int e1 = comp[1];
+          int s2 = intervals[i][0];
+          int e2 = intervals[i][1];
 
-          if(s2 >= s1 && s2 < e1)
+          if(s2 < e1)
           {
               minRemovals++;
 
               if(e2 < e1)
-              comp = in[i]; 
+              {
+                comp = intervals[i];
+              }
           }
           else
           {
-            comp = in[i];
+             comp = intervals[i];
           }
       }
 
@@ -125,3 +103,4 @@ class Solution {
            
     }
 }
+
