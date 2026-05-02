b27ae4567614c1d3e6167f603250afef3b52b391
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-02 13:04:06 +0530

    Time: 2 ms (27.40%) | Memory: 47.4 MB (6.84%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,14 +1,98 @@
-class Interval{
+// class Interval{
 
-    int start;
-    int end;
+//     int start;
+//     int end;
 
-    Interval(int start, int end)
-    {
-        this.start = start;
-        this.end = end;
-    }
-}
+//     Interval(int start, int end)
+//     {
+//         this.start = start;
+//         this.end = end;
+//     }
+// }
+
+// class Solution {
+//     public int[][] insert(int[][] intervals, int[] newInterval) {
+         
+//         int n = intervals.length;
+
+//         if(n == 0)
+//         {
+//             int[][] ans = new int[1][2];
+//             ans[0][0] = newInterval[0];
+//             ans[0][1] = newInterval[1];
+//             return ans;
+//         }
+
+//         List<Interval> finalIntervals = new ArrayList<>();
+
+//         for(int i=0;i<n;i++)
+//         {
+//             int start = intervals[i][0];
+//             int end = intervals[i][1];
+
+//             finalIntervals.add(new Interval(start,end));
+//         }
+        
+//         for(int i=0;i<n;i++)
+//         {
+//              if(newInterval[0] < finalIntervals.get(i).start)
+//              {
+//                 finalIntervals.add(i,new Interval(newInterval[0],newInterval[1]));
+//                 break;
+//              }
+//         }
+
+//         if(finalIntervals.size() != n + 1)
+//         finalIntervals.add(new Interval(newInterval[0],newInterval[1]));
+         
+
+
+//         Stack<Interval> st = new Stack<>();
+
+//         st.push(finalIntervals.get(0));
+//         int prevStart = finalIntervals.get(0).start;
+//         int prevEnd = finalIntervals.get(0).end;
+
+//         for(int i=1;i<finalIntervals.size();i++)
+//         {
+//             Interval currInterval = finalIntervals.get(i);
+
+//             int currStart = currInterval.start;
+//             int currEnd = currInterval.end;
+
+//             if(currStart <= prevEnd)
+//             {
+//                 st.pop();
+
+//                 Interval mergedInterval = new Interval(Math.min(prevStart,currStart),Math.max(prevEnd,currEnd));
+//                 st.push(mergedInterval);
+//                 prevStart = mergedInterval.start;
+//                 prevEnd = mergedInterval.end;
+//             }
+//             else
+//             {
+//                 st.push(currInterval);
+//                 prevStart = currStart;
+//                 prevEnd = currEnd;
+//             }
+//         }
+
+//         int[][] ans = new int[st.size()][2];
+
+//         for(int i=st.size()-1;i>=0;i--)
+//         {
+//              ans[i][0] = st.peek().start;
+//              ans[i][1] = st.peek().end;
+//              st.pop();
+//         }
+
+
+//      return ans;
+
+//     }
+// }
+
+
 
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
@@ -23,51 +107,46 @@ class Solution {
             return ans;
         }
 
-        List<Interval> finalIntervals = new ArrayList<>();
+        List<int[]> finalIntervals = new ArrayList<>();
 
         for(int i=0;i<n;i++)
-        {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            finalIntervals.add(new Interval(start,end));
-        }
+            finalIntervals.add(intervals[i]);
         
         for(int i=0;i<n;i++)
         {
-             if(newInterval[0] < finalIntervals.get(i).start)
+             if(newInterval[0] < finalIntervals.get(i)[0])
              {
-                finalIntervals.add(i,new Interval(newInterval[0],newInterval[1]));
+                finalIntervals.add(i,newInterval);
                 break;
              }
         }
 
         if(finalIntervals.size() != n + 1)
-        finalIntervals.add(new Interval(newInterval[0],newInterval[1]));
+        finalIntervals.add(newInterval);
          
 
 
-        Stack<Interval> st = new Stack<>();
+        Stack<int[]> st = new Stack<>();
 
         st.push(finalIntervals.get(0));
-        int prevStart = finalIntervals.get(0).start;
-        int prevEnd = finalIntervals.get(0).end;
+        int prevStart = st.peek()[0];
+        int prevEnd =  st.peek()[1];
 
         for(int i=1;i<finalIntervals.size();i++)
         {
-            Interval currInterval = finalIntervals.get(i);
+            int[] currInterval = finalIntervals.get(i);
 
-            int currStart = currInterval.start;
-            int currEnd = currInterval.end;
+            int currStart = currInterval[0];
+            int currEnd = currInterval[1];
 
             if(currStart <= prevEnd)
             {
                 st.pop();
 
-                Interval mergedInterval = new Interval(Math.min(prevStart,currStart),Math.max(prevEnd,currEnd));
+                int[] mergedInterval = new int[]{Math.min(prevStart,currStart),Math.max(prevEnd,currEnd)};
                 st.push(mergedInterval);
-                prevStart = mergedInterval.start;
-                prevEnd = mergedInterval.end;
+                prevStart = mergedInterval[0];
+                prevEnd = mergedInterval[1];
             }
             else
             {
@@ -81,8 +160,8 @@ class Solution {
 
         for(int i=st.size()-1;i>=0;i--)
         {
-             ans[i][0] = st.peek().start;
-             ans[i][1] = st.peek().end;
+             ans[i][0] = st.peek()[0];
+             ans[i][1] = st.peek()[1];
              st.pop();
         }
 
