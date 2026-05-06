7fe893bf83135c6c80f9bd32cef5787327a677c3
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-06 17:33:27 +0530

    Time: 4 ms (22.53%) | Memory: 47.6 MB (51.02%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -93,34 +93,99 @@
 // }
 
 
+// class Solution {
+//     public int candy(int[] ratings) {
+       
+//         int n = ratings.length;
+//         int[] candy = new int[n];
+
+//         candy[0] = 1;       
+//         for(int i=1;i<n;i++)
+//         {
+//             if(ratings[i] > ratings[i-1])
+//                 candy[i] = candy[i-1] + 1;
+//             else
+//                 candy[i] = 1;
+//         }
+
+//        int res = candy[n-1];
+
+//         for(int i=n-2;i>=0;i--)
+//         { 
+//              if(ratings[i] > ratings[i+1])
+//              candy[i] = Math.max(candy[i],candy[i+1] + 1);
+
+//              res += candy[i];
+//         }
+
+
+
+//        return res;
+
+//     }
+// }
+
+
+
+
+
+
 class Solution {
     public int candy(int[] ratings) {
        
         int n = ratings.length;
         int[] candy = new int[n];
 
-        candy[0] = 1;       
-        for(int i=1;i<n;i++)
-        {
-            if(ratings[i] > ratings[i-1])
-                candy[i] = candy[i-1] + 1;
-            else
+        if(n == 1)
+        return 1;
+
+        int res = 0;
+       
+       for(int i=0;i<n;i++)
+       {
+
+           if(i-1>=0)
+           {
+             if(ratings[i] > ratings[i-1])
+                candy[i] = candy[i-1]+1;
+             else
                 candy[i] = 1;
-        }
+           }
 
-       int res = candy[n-1];
-
-        for(int i=n-2;i>=0;i--)
-        { 
+           if(i+1<=n-1)
+           {
              if(ratings[i] > ratings[i+1])
-             candy[i] = Math.max(candy[i],candy[i+1] + 1);
+             {
+                int j = i+2;
 
-             res += candy[i];
-        }
+                while(j<n && ratings[j] < ratings[j-1])
+                j++;
 
+                int candies = j - i;
 
+                candy[i] = Math.max(candy[i],candies--);
+                res += candy[i];
+                 
+                for(int k=i+1;k<j;k++)
+                {
+                 candy[k] = candies--;
+                 res += candy[k];
+                }
+
+                 i = j - 1;
+                 continue;
+             }
+             else
+             {
+                candy[i] = Math.max(candy[i],1);
+             }
+           }
+
+           res += candy[i];
+       }
 
        return res;
+
 
     }
 }
