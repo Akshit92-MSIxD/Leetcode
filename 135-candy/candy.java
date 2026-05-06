88af7efb91acad0eeabb052526dc9e892f6af257
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-06 12:30:08 +0530

    Time: 4 ms (22.53%) | Memory: 47.5 MB (69.53%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,3 +1,58 @@
+// class Solution {
+//     public int candy(int[] ratings) {
+       
+//         int n = ratings.length;
+//         int[] candy = new int[n];
+
+//         if(n == 1)
+//         return 1;
+       
+//        for(int i=0;i<n;i++)
+//        {
+//            if(candy[i] != 0)
+//            continue;
+
+//            if(i-1>=0)
+//            {
+//              if(ratings[i] > ratings[i-1])
+//                 candy[i] = Math.max(candy[i],candy[i-1]+1);
+//              else
+//                 candy[i] = 1;
+//            }
+
+//            if(i+1<=n-1)
+//            {
+//              if(ratings[i] > ratings[i+1])
+//              {
+//                 int j = i+1;
+
+//                 while(j<n && ratings[j] < ratings[j-1])
+//                 j++;
+
+//                 int candies = j - i;
+
+//                 for(int k=i;k<j;k++)
+//                  candy[k] = Math.max(candy[k],candies--);
+//              }
+//              else
+//              {
+//                 candy[i] = Math.max(candy[i],1);
+//              }
+//            }
+//        }
+
+
+//        int minCandies = 0;
+
+//        for(int val : candy)
+//        minCandies+=val;
+
+//        return minCandies;
+
+//     }
+// }
+
+
 class Solution {
     public int candy(int[] ratings) {
        
@@ -9,8 +64,6 @@ class Solution {
        
        for(int i=0;i<n;i++)
        {
-           if(candy[i] != 0)
-           continue;
 
            if(i-1>=0)
            {
@@ -33,6 +86,8 @@ class Solution {
 
                 for(int k=i;k<j;k++)
                  candy[k] = Math.max(candy[k],candies--);
+
+                 i = j - 1;
              }
              else
              {
