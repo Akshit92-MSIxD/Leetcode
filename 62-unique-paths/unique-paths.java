9ff0e01802154a409b97c45cee48cde5d99477ce
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-10 12:36:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (28.12%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,37 +1,55 @@
-// class Solution {
-//     public int uniquePaths(int m, int n) {
-        
-//           int[][] dp = new int[m][n];
+// Note: I have written two approaches for this problem below. Please read both of them !!!
 
-//           for(int j=0;j<n;j++)
-//           dp[0][j] = 1;
 
-//           for(int i=0;i<m;i++)
-//           dp[i][0] = 1;
-
-//           for(int i=1;i<m;i++)
-//             for(int j=1;j<n;j++)
-//             dp[i][j] = dp[i-1][j] + dp[i][j-1];
-
-//             return dp[m-1][n-1];
-
-//     }
-// }
-
+// Approach 1 : DP Tabulation(Iterative)
+// TC : O(m*n)
+// SC : O(m*n)
 
 class Solution {
     public int uniquePaths(int m, int n) {
         
-          int[] dp = new int[n];
+          int[][] dp = new int[m][n];
 
           for(int j=0;j<n;j++)
-          dp[j] = 1;
+          dp[0][j] = 1;
+
+          for(int i=0;i<m;i++)
+          dp[i][0] = 1;
 
           for(int i=1;i<m;i++)
             for(int j=1;j<n;j++)
-            dp[j] = dp[j] + dp[j-1];
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
 
-            return dp[n-1];
+            return dp[m-1][n-1];
 
     }
 }
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+// // Approach 2 : Optmized version of Approach 1
+// // TC : O(m*n)
+// // SC : O(n)
+
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+        
+//           int[] dp = new int[n];
+
+//           for(int j=0;j<n;j++)
+//           dp[j] = 1;
+
+//           for(int i=1;i<m;i++)
+//             for(int j=1;j<n;j++)
+//             dp[j] = dp[j] + dp[j-1];
+
+//             return dp[n-1];
+
+//     }
+// }
