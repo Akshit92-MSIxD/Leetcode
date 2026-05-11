8b917d8b9cc71f664a9a471f2be0572e881cc833
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-11 17:58:12 +0530

    Time: 4 ms (46.71%) | Memory: 45.4 MB (73.08%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,84 +1,64 @@
-// class Solution {
-//     public int minimumTotal(List<List<Integer>> triangle) {
+// Note : I have written three DP approaches for this problem below . Please read from top to bottom for better understanding
+
+// We have indirectly used the concept of rolling array here in Approach 1 and 2 without knowing about it :) lol
+
+
+
+// Approach 1 : DP Tabulation(Iterative)
+// TC : O(n^2)
+// SC : O(n^2)
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
           
-//         int m = triangle.size();
-//         int n = triangle.get(m-1).size();
+        int m = triangle.size();
+        int n = triangle.get(m-1).size();
 
-//         int[][] dp = new int[m][];
+        int[][] dp = new int[m][];
 
-//         for(int i=0;i<m;i++)
-//         dp[i] = new int[i+1];
+        for(int i=0;i<m;i++)
+        dp[i] = new int[i+1];
 
-//         dp[0][0] = triangle.get(0).get(0);
+        dp[0][0] = triangle.get(0).get(0);
 
-//         for(int i=1;i<m;i++)
-//         {
-//             for(int j=0;j<dp[i].length;j++)
-//             {
-//                if(j==0)
-//                 dp[i][j] = dp[i-1][j] + triangle.get(i).get(j);
-//                else if(j == dp[i].length-1)
-//                  dp[i][j] = dp[i-1][j-1] + triangle.get(i).get(j);
-//                else
-//                  dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j-1]) + triangle.get(i).get(j);
-//             }
-//         }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=0;j<dp[i].length;j++)
+            {
+               if(j==0)
+                dp[i][j] = dp[i-1][j] + triangle.get(i).get(j);
+               else if(j == dp[i].length-1)
+                 dp[i][j] = dp[i-1][j-1] + triangle.get(i).get(j);
+               else
+                 dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j-1]) + triangle.get(i).get(j);
+            }
+        }
 
-//         int minPath = Integer.MAX_VALUE;
+        int minPath = Integer.MAX_VALUE;
 
-//         for(int j=0;j<n;j++)
-//         {
-//             if(dp[m-1][j] < minPath)
-//             minPath = dp[m-1][j];
-//         }
+        for(int j=0;j<n;j++)
+        {
+            if(dp[m-1][j] < minPath)
+            minPath = dp[m-1][j];
+        }
 
-//         return minPath;
+        return minPath;
            
-//     }
-// }
+    }
+}
 
 
 
-// class Solution {
-//     public int minimumTotal(List<List<Integer>> triangle) {
-          
-//         int m = triangle.size();
-//         int n = triangle.get(m-1).size();
 
-//         int[] prev = new int[n];
-
-//         prev[0] = triangle.get(0).get(0);
-
-//         for(int i=1;i<m;i++)
-//         {
-//                 int[] curr = new int[n];
-//             for(int j=0;j<triangle.get(i).size();j++)
-//             {
-//                if(j==0)
-//                 curr[j] = prev[j] + triangle.get(i).get(j);
-//                else if(j == triangle.get(i).size() - 1)
-//                  curr[j] = prev[j-1] + triangle.get(i).get(j);
-//                else
-//                  curr[j] = Math.min(prev[j],prev[j-1]) + triangle.get(i).get(j);
-//             }
-
-//             prev = curr;
-//         }
-
-//         int minPath = Integer.MAX_VALUE;
-
-//         for(int j=0;j<n;j++)
-//         {
-//             if(prev[j] < minPath)
-//             minPath = prev[j];
-//         }
-
-//         return minPath;
-           
-//     }
-// }
+/*-----------------------------------------------------------------------------------------------------------*/
 
 
+
+
+// // Approach 2 : Space Optimized Version of Approach 1 [Using only two 1D arrays curr and prev !!!]
+// // Note : Here we have make use of rolling array !!!
+// // TC : O(n^2)
+// // SC : O(n)
 
 // class Solution {
 //     public int minimumTotal(List<List<Integer>> triangle) {
@@ -119,38 +99,52 @@
 // }
 
 
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
+
+
+
+/*------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+// // Approach 3 : Space Optimized Version of Approach 1 [Using only one 1D array !!!] + Traverse from R to L !!!
+// // Note : Here we have make use of rolling array !!!
+// // TC : O(n^2)
+// // SC : O(n)
+
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
           
-        int m = triangle.size();
-        int n = triangle.get(m-1).size();
+//         int m = triangle.size();
+//         int n = triangle.get(m-1).size();
 
-        int[] dp = new int[n];
+//         int[] dp = new int[n];
 
-        dp[0] = triangle.get(0).get(0);
+//         dp[0] = triangle.get(0).get(0); 
 
-        for(int i=1;i<m;i++)
-        {
-            for(int j = triangle.get(i).size() - 1;j>=0;j--)
-            {
-               if(j==0)
-                dp[j] = dp[j] + triangle.get(i).get(j);
-               else if(j == triangle.get(i).size() - 1)
-                 dp[j] = dp[j-1] + triangle.get(i).get(j);
-               else
-                 dp[j] = Math.min(dp[j],dp[j-1]) + triangle.get(i).get(j);
-            }
-        }
+//         for(int i=1;i<m;i++)
+//         {
+//              // traversing from right to left makes the use of only one array possible and valid !!!
+//             for(int j = triangle.get(i).size() - 1;j>=0;j--)
+//             {
+//                if(j==0)
+//                 dp[j] = dp[j] + triangle.get(i).get(j);
+//                else if(j == triangle.get(i).size() - 1)
+//                  dp[j] = dp[j-1] + triangle.get(i).get(j);
+//                else
+//                  dp[j] = Math.min(dp[j],dp[j-1]) + triangle.get(i).get(j);
+//             }
+//         }
 
-        int minPath = Integer.MAX_VALUE;
+//         int minPath = Integer.MAX_VALUE;
 
-        for(int j=0;j<dp.length;j++)
-        {
-            if(dp[j] < minPath)
-            minPath = dp[j];
-        }
+//         for(int j=0;j<dp.length;j++)
+//         {
+//             if(dp[j] < minPath)
+//             minPath = dp[j];
+//         }
 
-        return minPath;
+//         return minPath;
            
-    }
-}
+//     }
+// }
