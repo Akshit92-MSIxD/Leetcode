1a74ee3180e8043075a403071ec0bb2d2853b641
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-11 17:25:31 +0530

    Time: 6 ms (25.04%) | Memory: 45.4 MB (60.83%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,35 +1,76 @@
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+          
+//         int m = triangle.size();
+//         int n = triangle.get(m-1).size();
+
+//         int[][] dp = new int[m][];
+
+//         for(int i=0;i<m;i++)
+//         dp[i] = new int[i+1];
+
+//         dp[0][0] = triangle.get(0).get(0);
+
+//         for(int i=1;i<m;i++)
+//         {
+//             for(int j=0;j<dp[i].length;j++)
+//             {
+//                if(j==0)
+//                 dp[i][j] = dp[i-1][j] + triangle.get(i).get(j);
+//                else if(j == dp[i].length-1)
+//                  dp[i][j] = dp[i-1][j-1] + triangle.get(i).get(j);
+//                else
+//                  dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j-1]) + triangle.get(i).get(j);
+//             }
+//         }
+
+//         int minPath = Integer.MAX_VALUE;
+
+//         for(int j=0;j<n;j++)
+//         {
+//             if(dp[m-1][j] < minPath)
+//             minPath = dp[m-1][j];
+//         }
+
+//         return minPath;
+           
+//     }
+// }
+
+
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
           
         int m = triangle.size();
         int n = triangle.get(m-1).size();
 
-        int[][] dp = new int[m][];
+        int[] prev = new int[n];
 
-        for(int i=0;i<m;i++)
-        dp[i] = new int[i+1];
-
-        dp[0][0] = triangle.get(0).get(0);
+        prev[0] = triangle.get(0).get(0);
 
         for(int i=1;i<m;i++)
         {
-            for(int j=0;j<dp[i].length;j++)
+                int[] curr = new int[n];
+            for(int j=0;j<triangle.get(i).size();j++)
             {
                if(j==0)
-                dp[i][j] = dp[i-1][j] + triangle.get(i).get(j);
-               else if(j == dp[i].length-1)
-                 dp[i][j] = dp[i-1][j-1] + triangle.get(i).get(j);
+                curr[j] = prev[j] + triangle.get(i).get(j);
+               else if(j == triangle.get(i).size() - 1)
+                 curr[j] = prev[j-1] + triangle.get(i).get(j);
                else
-                 dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j-1]) + triangle.get(i).get(j);
+                 curr[j] = Math.min(prev[j],prev[j-1]) + triangle.get(i).get(j);
             }
+
+            prev = curr;
         }
 
         int minPath = Integer.MAX_VALUE;
 
         for(int j=0;j<n;j++)
         {
-            if(dp[m-1][j] < minPath)
-            minPath = dp[m-1][j];
+            if(prev[j] < minPath)
+            minPath = prev[j];
         }
 
         return minPath;
