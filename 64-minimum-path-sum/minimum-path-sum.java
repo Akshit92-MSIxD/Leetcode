f546a57238bbc2c7463f6667e51ccf62ed76bf74
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-11 13:58:25 +0530

    Time: 6 ms (9.87%) | Memory: 50.4 MB (38.69%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,10 +1,46 @@
+// class Solution {
+//     public int minPathSum(int[][] grid) {
+         
+//          int m = grid.length;
+//          int n = grid[0].length;
+
+//          int[][] dp = new int[m][n];
+
+//          for(int i=0;i<m;i++)
+//          {
+//             for(int j=0;j<n;j++)
+//             {
+//                 int minPathSum = Integer.MAX_VALUE;
+
+//                 if(i-1>=0)
+//                 minPathSum = Math.min(minPathSum,dp[i-1][j]);
+
+//                 if(j-1>=0)
+//                 minPathSum = Math.min(minPathSum,dp[i][j-1]);
+                
+//                 if(minPathSum == Integer.MAX_VALUE)
+//                 dp[i][j] = grid[i][j];
+//                 else
+//                 dp[i][j] = grid[i][j] + minPathSum;
+
+//             }
+//          }
+
+
+//          return dp[m-1][n-1];
+
+
+//     }
+// }
+
+
 class Solution {
     public int minPathSum(int[][] grid) {
          
          int m = grid.length;
          int n = grid[0].length;
 
-         int[][] dp = new int[m][n];
+         int[] dp = new int[n];
 
          for(int i=0;i<m;i++)
          {
@@ -13,21 +49,21 @@ class Solution {
                 int minPathSum = Integer.MAX_VALUE;
 
                 if(i-1>=0)
-                minPathSum = Math.min(minPathSum,dp[i-1][j]);
+                minPathSum = Math.min(minPathSum,dp[j]);
 
                 if(j-1>=0)
-                minPathSum = Math.min(minPathSum,dp[i][j-1]);
+                minPathSum = Math.min(minPathSum,dp[j-1]);
                 
                 if(minPathSum == Integer.MAX_VALUE)
-                dp[i][j] = grid[i][j];
+                dp[j] = grid[i][j];
                 else
-                dp[i][j] = grid[i][j] + minPathSum;
+                dp[j] = grid[i][j] + minPathSum;
 
             }
          }
 
 
-         return dp[m-1][n-1];
+         return dp[n-1];
 
 
     }
