878998bfcc8e1a12cdebd22a62121ae49be6c816
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-12 11:28:42 +0530

    Time: 7 ms (57.05%) | Memory: 47.9 MB (6.74%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -36,40 +36,73 @@
 // }
 
 
+// class Solution {
+//     public int minFallingPathSum(int[][] matrix) {
+
+//         int n = matrix.length;
+        
+//         int[] prev = new int[n];
+
+//         for(int j=0;j<n;j++)
+//          prev[j] = matrix[0][j];
+
+        
+//         for(int i=1;i<n;i++)
+//         {
+//             int[] curr = new int[n];
+
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(j==0)
+//                   curr[j] = Math.min(prev[j],prev[j+1]) + matrix[i][j];
+//                 else if(j == n-1)
+//                   curr[j] = Math.min(prev[j-1],prev[j]) + matrix[i][j];
+//                 else
+//                  curr[j] = Math.min(prev[j-1],Math.min(prev[j],prev[j+1]))+ matrix[i][j];
+//             }
+
+//             prev = curr;
+//         }
+
+//         int minSum = Integer.MAX_VALUE;
+
+//         for(int j=0;j<n;j++)
+//         {
+//             if(prev[j] < minSum)
+//             minSum = prev[j];
+//         }
+
+//         return minSum;
+        
+//     }
+// }
+
+
+
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
 
         int n = matrix.length;
-        
-        int[] prev = new int[n];
 
-        for(int j=0;j<n;j++)
-         prev[j] = matrix[0][j];
-
-        
         for(int i=1;i<n;i++)
         {
-            int[] curr = new int[n];
-
             for(int j=0;j<n;j++)
             {
                 if(j==0)
-                  curr[j] = Math.min(prev[j],prev[j+1]) + matrix[i][j];
+                  matrix[i][j] += Math.min(matrix[i-1][j],matrix[i-1][j+1]);
                 else if(j == n-1)
-                  curr[j] = Math.min(prev[j-1],prev[j]) + matrix[i][j];
+                  matrix[i][j] += Math.min(matrix[i-1][j-1],matrix[i-1][j]);
                 else
-                 curr[j] = Math.min(prev[j-1],Math.min(prev[j],prev[j+1]))+ matrix[i][j];
+                 matrix[i][j] += Math.min(matrix[i-1][j-1],Math.min(matrix[i-1][j],matrix[i-1][j+1]));
             }
-
-            prev = curr;
         }
 
         int minSum = Integer.MAX_VALUE;
 
         for(int j=0;j<n;j++)
         {
-            if(prev[j] < minSum)
-            minSum = prev[j];
+            if(matrix[n-1][j] < minSum)
+            minSum = matrix[n-1][j];
         }
 
         return minSum;
