7dd9bd11ee97d8dc4660493298163900470d50be
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-12 11:40:54 +0530

    Time: 6 ms (68.41%) | Memory: 47.3 MB (47.49%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,39 +1,61 @@
-// class Solution {
-//     public int minFallingPathSum(int[][] matrix) {
+// Note: I have written three approches for this problem below. Please read from top to bottom for better understanding !!!
 
-//         int n = matrix.length;
+
+
+// Approach 1 : Using DP Tabulation(Iterative)
+// TC : O(n^2)
+// SC : O(n^2)
+
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+
+        int n = matrix.length;
         
-//         int[][] dp = new int[n][n];
+        int[][] dp = new int[n][n];
 
-//         for(int j=0;j<n;j++)
-//         dp[0][j] = matrix[0][j];
+        for(int j=0;j<n;j++)
+        dp[0][j] = matrix[0][j];
 
         
-//         for(int i=1;i<n;i++)
-//         {
-//             for(int j=0;j<n;j++)
-//             {
-//                 if(j==0)
-//                   dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j+1]) + matrix[i][j];
-//                 else if(j == n-1)
-//                   dp[i][j] = Math.min(dp[i-1][j-1],dp[i-1][j]) + matrix[i][j];
-//                 else
-//                   dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i-1][j+1]))+ matrix[i][j];
-//             }
-//         }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==0)
+                  dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j+1]) + matrix[i][j];
+                else if(j == n-1)
+                  dp[i][j] = Math.min(dp[i-1][j-1],dp[i-1][j]) + matrix[i][j];
+                else
+                  dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i-1][j+1]))+ matrix[i][j];
+            }
+        }
 
-//         int minSum = Integer.MAX_VALUE;
+        int minSum = Integer.MAX_VALUE;
 
-//         for(int j=0;j<n;j++)
-//         {
-//             if(dp[n-1][j] < minSum)
-//             minSum = dp[n-1][j];
-//         }
+        for(int j=0;j<n;j++)
+        {
+            if(dp[n-1][j] < minSum)
+            minSum = dp[n-1][j];
+        }
 
-//         return minSum;
+        return minSum;
         
-//     }
-// }
+    }
+}
+
+
+
+
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+// // Approach 2 : Space optmized version of Approach 1 (using rolling DP)
+// // TC : O(n^2)
+// // SC : O(n)
 
 
 // class Solution {
@@ -79,33 +101,47 @@
 
 
 
-class Solution {
-    public int minFallingPathSum(int[][] matrix) {
 
-        int n = matrix.length;
 
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(j==0)
-                  matrix[i][j] += Math.min(matrix[i-1][j],matrix[i-1][j+1]);
-                else if(j == n-1)
-                  matrix[i][j] += Math.min(matrix[i-1][j-1],matrix[i-1][j]);
-                else
-                 matrix[i][j] += Math.min(matrix[i-1][j-1],Math.min(matrix[i-1][j],matrix[i-1][j+1]));
-            }
-        }
+/*---------------------------------------------------------------------------------------------------------------*/
 
-        int minSum = Integer.MAX_VALUE;
 
-        for(int j=0;j<n;j++)
-        {
-            if(matrix[n-1][j] < minSum)
-            minSum = matrix[n-1][j];
-        }
 
-        return minSum;
+
+
+// // Approach 3 : Updates the matrix in-place !!! (works only when we are allowed to change original input array)
+// // Note : This approach is derived from DP tabulation !!!
+// // TC : O(n^2)
+// // SC : O(1)
+
+
+// class Solution {
+//     public int minFallingPathSum(int[][] matrix) {
+
+//         int n = matrix.length;
+
+//         for(int i=1;i<n;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(j==0)
+//                   matrix[i][j] += Math.min(matrix[i-1][j],matrix[i-1][j+1]);
+//                 else if(j == n-1)
+//                   matrix[i][j] += Math.min(matrix[i-1][j-1],matrix[i-1][j]);
+//                 else
+//                  matrix[i][j] += Math.min(matrix[i-1][j-1],Math.min(matrix[i-1][j],matrix[i-1][j+1]));
+//             }
+//         }
+
+//         int minSum = Integer.MAX_VALUE;
+
+//         for(int j=0;j<n;j++)
+//         {
+//             if(matrix[n-1][j] < minSum)
+//             minSum = matrix[n-1][j];
+//         }
+
+//         return minSum;
         
-    }
-}
+//     }
+// }
