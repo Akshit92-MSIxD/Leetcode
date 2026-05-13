5c78bf5923fdc948759d441311a1c86f081e8276
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-13 11:26:39 +0530

    Time: 28 ms (34.78%) | Memory: 46.6 MB (36.50%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,48 +1,66 @@
-// class Solution {
-//     public int cherryPickup(int[][] grid) {
+//Note : I have written two approaches for this problem below. Please read from top to bottom for better understanding !!!
 
-//         int rows = grid.length;
-//         int cols = grid[0].length;
 
-//         int[][][] dp = new int[rows][cols][cols];
 
-//         for(int j1=0;j1<cols;j1++)
-//         {
-//             for(int j2=j1+1;j2<cols;j2++)
-//                 dp[rows-1][j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
-//         }
+// Approach 1 : Using 3D DP Tabulation(Iterative)
+// TC : O(m*n*n)
+// SC : O(m*n*n)
+
+class Solution {
+    public int cherryPickup(int[][] grid) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int[][][] dp = new int[rows][cols][cols];
+
+        for(int j1=0;j1<cols;j1++)
+        {
+            for(int j2=j1+1;j2<cols;j2++)
+                dp[rows-1][j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
+        }
         
-//         int[] moveOprs = {-1,0,1};
+        int[] moveOprs = {-1,0,1};
 
-//        for(int i=rows-2;i>=0;i--)
-//        {
-//          for(int j1=0;j1<cols;j1++)
-//          {
-//             for(int j2=j1+1;j2<cols;j2++)
-//             {
-//                   for(int mv1 : moveOprs)
-//                   {
-//                      for(int mv2 : moveOprs)
-//                      {
-//                          if(j1+mv1 >=0 && j1+mv1 < cols && j2+mv2 >= 0 && j2+mv2 < cols && j1+mv1 < j2+mv2)
-//                          dp[i][j1][j2] = Math.max(dp[i][j1][j2],dp[i+1][j1+mv1][j2+mv2]);
-//                      }
-//                   }
+       for(int i=rows-2;i>=0;i--)
+       {
+         for(int j1=0;j1<cols;j1++)
+         {
+            for(int j2=j1+1;j2<cols;j2++)
+            {
+                  for(int mv1 : moveOprs)
+                  {
+                     for(int mv2 : moveOprs)
+                     {
+                         if(j1+mv1 >=0 && j1+mv1 < cols && j2+mv2 >= 0 && j2+mv2 < cols && j1+mv1 < j2+mv2)
+                         dp[i][j1][j2] = Math.max(dp[i][j1][j2],dp[i+1][j1+mv1][j2+mv2]);
+                     }
+                  }
 
-//                   dp[i][j1][j2] += (grid[i][j1] + grid[i][j2]);
-//             }
-//          }
-//        }
+                  dp[i][j1][j2] += (grid[i][j1] + grid[i][j2]);
+            }
+         }
+       }
       
 
-//       return dp[0][0][cols-1];
+      return dp[0][0][cols-1];
 
 
-//     }
-// }
+    }
+}
 
 
 
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+// // Approach 2 : Space Optmized Version of Approach 1 (Using Rolling DP)
+// // TC : O(m*n*n)
+// // SC : O(n*n)
 
 // class Solution {
 //     public int cherryPickup(int[][] grid) {
@@ -58,7 +76,6 @@
 //                 prev[j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
 //         }
         
-//         int[] moveOprs = {-1,0,1};
 
 //        for(int i=rows-2;i>=0;i--)
 //        { 
@@ -68,12 +85,15 @@
 //          {
 //             for(int j2=j1+1;j2<cols;j2++)
 //             {
-//                   for(int mv1 : moveOprs)
+//                   for(int mv1 = -1; mv1<=1 ; mv1++)
 //                   {
-//                      for(int mv2 : moveOprs)
+//                      for(int mv2 = -1; mv2<=1 ; mv2++)
 //                      {
-//                          if(j1+mv1 >=0 && j1+mv1 < cols && j2+mv2 >= 0 && j2+mv2 < cols && j1+mv1 < j2+mv2)
-//                          curr[j1][j2] = Math.max(curr[j1][j2],prev[j1+mv1][j2+mv2]);
+//                          int corr_j1 = j1+mv1;
+//                          int corr_j2 = j2+mv2;
+
+//                          if(corr_j1 >=0 && corr_j1 < cols && corr_j2 >= 0 && corr_j2 < cols && corr_j1 < corr_j2)
+//                          curr[j1][j2] = Math.max(curr[j1][j2],prev[corr_j1][corr_j2]);
 //                      }
 //                   }
 
@@ -90,57 +110,6 @@
 
 //     }
 // }
-
-
-class Solution {
-    public int cherryPickup(int[][] grid) {
-
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        int[][] prev = new int[cols][cols];
-
-        for(int j1=0;j1<cols;j1++)
-        {
-            for(int j2=j1+1;j2<cols;j2++)
-                prev[j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
-        }
-        
-        int[] moveOprs = {-1,0,1};
-
-       for(int i=rows-2;i>=0;i--)
-       { 
-          int[][] curr = new int[cols][cols];
-
-         for(int j1=0;j1<cols;j1++)
-         {
-            for(int j2=j1+1;j2<cols;j2++)
-            {
-                  for(int mv1 : moveOprs)
-                  {
-                     for(int mv2 : moveOprs)
-                     {
-                         int corr_j1 = j1+mv1;
-                         int corr_j2 = j2+mv2;
-
-                         if(corr_j1 >=0 && corr_j1 < cols && corr_j2 >= 0 && corr_j2 < cols && corr_j1 < corr_j2)
-                         curr[j1][j2] = Math.max(curr[j1][j2],prev[corr_j1][corr_j2]);
-                     }
-                  }
-
-                  curr[j1][j2] += (grid[i][j1] + grid[i][j2]);
-            }
-         }
-
-         prev = curr;
-       }
-      
-
-      return prev[0][cols-1];
-
-
-    }
-}
 
 
 
