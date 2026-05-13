359855068b49919f204f1ffc2b43261b9e62c08a
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-13 11:18:09 +0530

    Time: 25 ms (65.64%) | Memory: 46.3 MB (62.78%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -93,7 +93,6 @@
 
 
 class Solution {
-
     public int cherryPickup(int[][] grid) {
 
         int rows = grid.length;
@@ -101,48 +100,47 @@ class Solution {
 
         int[][] prev = new int[cols][cols];
 
-        // Base case
-        for (int j1 = 0; j1 < cols; j1++) {
-            for (int j2 = j1 + 1; j2 < cols; j2++) {
-                prev[j1][j2] = grid[rows - 1][j1] + grid[rows - 1][j2];
-            }
+        for(int j1=0;j1<cols;j1++)
+        {
+            for(int j2=j1+1;j2<cols;j2++)
+                prev[j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
         }
+        
+        int[] moveOprs = {-1,0,1};
 
-        for (int i = rows - 2; i >= 0; i--) {
+       for(int i=rows-2;i>=0;i--)
+       { 
+          int[][] curr = new int[cols][cols];
 
-            int[][] curr = new int[cols][cols];
+         for(int j1=0;j1<cols;j1++)
+         {
+            for(int j2=j1+1;j2<cols;j2++)
+            {
+                  for(int mv1 : moveOprs)
+                  {
+                     for(int mv2 : moveOprs)
+                     {
+                         int corr_j1 = j1+mv1;
+                         int corr_j2 = j2+mv2;
 
-            for (int j1 = 0; j1 < cols; j1++) {
+                         if(corr_j1 >=0 && corr_j1 < cols && corr_j2 >= 0 && corr_j2 < cols && corr_j1 < corr_j2)
+                         curr[j1][j2] = Math.max(curr[j1][j2],prev[corr_j1][corr_j2]);
+                     }
+                  }
 
-                for (int j2 = j1 + 1; j2 < cols; j2++) {
-
-                    int best = 0;
-
-                    for (int mv1 = -1; mv1 <= 1; mv1++) {
-
-                        int nj1 = j1 + mv1;
-
-                        if (nj1 < 0 || nj1 >= cols)
-                            continue;
-
-                        for (int mv2 = -1; mv2 <= 1; mv2++) {
-
-                            int nj2 = j2 + mv2;
-
-                            if (nj2 < 0 || nj2 >= cols || nj1 >= nj2)
-                                continue;
-
-                            best = Math.max(best, prev[nj1][nj2]);
-                        }
-                    }
-
-                    curr[j1][j2] = best + grid[i][j1] + grid[i][j2];
-                }
+                  curr[j1][j2] += (grid[i][j1] + grid[i][j2]);
             }
+         }
 
-            prev = curr;
-        }
+         prev = curr;
+       }
+      
 
-        return prev[0][cols - 1];
+      return prev[0][cols-1];
+
+
     }
 }
+
+
+
