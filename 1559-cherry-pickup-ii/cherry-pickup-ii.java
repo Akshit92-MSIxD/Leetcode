17f7bff7044eaa066be6c450bfa5236addf49606
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-13 11:13:37 +0530

    Time: 12 ms (99.64%) | Memory: 46.6 MB (28.95%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -44,7 +44,56 @@
 
 
 
+// class Solution {
+//     public int cherryPickup(int[][] grid) {
+
+//         int rows = grid.length;
+//         int cols = grid[0].length;
+
+//         int[][] prev = new int[cols][cols];
+
+//         for(int j1=0;j1<cols;j1++)
+//         {
+//             for(int j2=j1+1;j2<cols;j2++)
+//                 prev[j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
+//         }
+        
+//         int[] moveOprs = {-1,0,1};
+
+//        for(int i=rows-2;i>=0;i--)
+//        { 
+//           int[][] curr = new int[cols][cols];
+
+//          for(int j1=0;j1<cols;j1++)
+//          {
+//             for(int j2=j1+1;j2<cols;j2++)
+//             {
+//                   for(int mv1 : moveOprs)
+//                   {
+//                      for(int mv2 : moveOprs)
+//                      {
+//                          if(j1+mv1 >=0 && j1+mv1 < cols && j2+mv2 >= 0 && j2+mv2 < cols && j1+mv1 < j2+mv2)
+//                          curr[j1][j2] = Math.max(curr[j1][j2],prev[j1+mv1][j2+mv2]);
+//                      }
+//                   }
+
+//                   curr[j1][j2] += (grid[i][j1] + grid[i][j2]);
+//             }
+//          }
+
+//          prev = curr;
+//        }
+      
+
+//       return prev[0][cols-1];
+
+
+//     }
+// }
+
+
 class Solution {
+
     public int cherryPickup(int[][] grid) {
 
         int rows = grid.length;
@@ -52,41 +101,48 @@ class Solution {
 
         int[][] prev = new int[cols][cols];
 
-        for(int j1=0;j1<cols;j1++)
-        {
-            for(int j2=j1+1;j2<cols;j2++)
-                prev[j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
-        }
-        
-        int[] moveOprs = {-1,0,1};
-
-       for(int i=rows-2;i>=0;i--)
-       { 
-          int[][] curr = new int[cols][cols];
-
-         for(int j1=0;j1<cols;j1++)
-         {
-            for(int j2=j1+1;j2<cols;j2++)
-            {
-                  for(int mv1 : moveOprs)
-                  {
-                     for(int mv2 : moveOprs)
-                     {
-                         if(j1+mv1 >=0 && j1+mv1 < cols && j2+mv2 >= 0 && j2+mv2 < cols && j1+mv1 < j2+mv2)
-                         curr[j1][j2] = Math.max(curr[j1][j2],prev[j1+mv1][j2+mv2]);
-                     }
-                  }
-
-                  curr[j1][j2] += (grid[i][j1] + grid[i][j2]);
+        // Base case
+        for (int j1 = 0; j1 < cols; j1++) {
+            for (int j2 = j1 + 1; j2 < cols; j2++) {
+                prev[j1][j2] = grid[rows - 1][j1] + grid[rows - 1][j2];
             }
-         }
+        }
 
-         prev = curr;
-       }
-      
+        for (int i = rows - 2; i >= 0; i--) {
 
-      return prev[0][cols-1];
+            int[][] curr = new int[cols][cols];
 
+            for (int j1 = 0; j1 < cols; j1++) {
 
+                for (int j2 = j1 + 1; j2 < cols; j2++) {
+
+                    int best = 0;
+
+                    for (int mv1 = -1; mv1 <= 1; mv1++) {
+
+                        int nj1 = j1 + mv1;
+
+                        if (nj1 < 0 || nj1 >= cols)
+                            continue;
+
+                        for (int mv2 = -1; mv2 <= 1; mv2++) {
+
+                            int nj2 = j2 + mv2;
+
+                            if (nj2 < 0 || nj2 >= cols || nj1 >= nj2)
+                                continue;
+
+                            best = Math.max(best, prev[nj1][nj2]);
+                        }
+                    }
+
+                    curr[j1][j2] = best + grid[i][j1] + grid[i][j2];
+                }
+            }
+
+            prev = curr;
+        }
+
+        return prev[0][cols - 1];
     }
 }
