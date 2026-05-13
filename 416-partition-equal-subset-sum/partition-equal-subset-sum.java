0e2a7322db327e241d0a5880fb1f9c20b735e867
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-13 14:17:13 +0530

    Time: 61 ms (37.47%) | Memory: 43.7 MB (92.62%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,3 +1,44 @@
+// class Solution {
+//     public boolean canPartition(int[] nums) {
+         
+//          int sum = 0;
+
+//          for(int val : nums)
+//          sum+=val;
+
+//          if(sum%2 != 0)
+//          return false;
+
+//          int target = sum/2;
+
+//          int n = nums.length;
+
+//          boolean[][] dp = new boolean[n][target+1];
+
+//          for(int i=0;i<n;i++)
+//          dp[i][0] = true;
+
+//          for(int j=1;j<=target;j++)
+//          dp[n-1][j] = (nums[n-1] == j);
+
+//          for(int i=n-2;i>=0;i--)
+//          {
+//             for(int j=1;j<=target;j++)
+//             {
+//                 dp[i][j] = dp[i+1][j]; // notPick;
+
+//                 if(nums[i]<=j)
+//                 dp[i][j] = dp[i][j] || dp[i+1][j-nums[i]];  // Pick
+//             }
+//          }
+
+
+//          return dp[0][target];
+
+// }
+// }
+
+
 class Solution {
     public boolean canPartition(int[] nums) {
          
@@ -13,27 +54,26 @@ class Solution {
 
          int n = nums.length;
 
-         boolean[][] dp = new boolean[n][target+1];
+         boolean[] dp = new boolean[target+1];
 
-         for(int i=0;i<n;i++)
-         dp[i][0] = true;
+         dp[0] = true;
 
          for(int j=1;j<=target;j++)
-         dp[n-1][j] = (nums[n-1] == j);
+         dp[j] = (nums[n-1] == j);
 
          for(int i=n-2;i>=0;i--)
          {
-            for(int j=1;j<=target;j++)
+            for(int j=target;j>=0;j--)
             {
-                dp[i][j] = dp[i+1][j]; // notPick;
+                dp[j] = dp[j]; // notPick;
 
                 if(nums[i]<=j)
-                dp[i][j] = dp[i][j] || dp[i+1][j-nums[i]];  // Pick
+                dp[j] = dp[j] || dp[j-nums[i]];  // Pick
             }
          }
 
 
-         return dp[0][target];
+         return dp[target];
 
 }
 }
