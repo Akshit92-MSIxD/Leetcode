aff1947df19127805e9323b524a8a5b78f2f625a
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-05-15 11:12:36 +0530

    Time: 12 ms (93.36%) | Memory: 46.1 MB (82.19%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+         int n = coins.length;
+
+         int[] dp = new int[amount+1];
+
+         dp[0] = 0;
+
+         for(int j=1;j<=amount;j++)
+         {
+            if(j%coins[n-1] == 0)
+            dp[j] = j/coins[n-1];
+            else
+            dp[j] = (int)1e6;
+         }
+
+         for(int i=n-2;i>=0;i--)
+            for(int j=1;j<=amount;j++)
+                if(coins[i] <= j) dp[j] = Math.min(dp[j],1 + dp[j-coins[i]]);
+
+
+         if(dp[amount] == (int)1e6)
+         return -1;
+
+         return dp[amount];
+    }
+}
