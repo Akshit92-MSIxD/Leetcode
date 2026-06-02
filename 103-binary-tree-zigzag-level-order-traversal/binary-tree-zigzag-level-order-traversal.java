517d27b6f07d4ec9f19461f5864526e1a53ee4a9
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-02 13:56:39 +0530

    Time: 1 ms (78.99%) | Memory: 43.9 MB (23.83%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,6 +14,71 @@
  * }
  */
 
+
+// Note : I have written two approaches for this problem below . Please read both of them !!!
+
+
+// Approach 2 : Using Normal BFS
+// TC : O(V)
+// Auxiliary SC : O(V) 
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+         
+      if(root == null)
+      return new ArrayList<>();
+
+      List<List<Integer>> order = new ArrayList<>();
+
+      Queue<TreeNode> q = new ArrayDeque<>();
+
+      int currLevel = 0;
+
+      q.add(root);
+
+      while(!q.isEmpty())
+      {
+          int currLevelSize = q.size();
+          List<Integer> currLevelValues = new ArrayList<>();
+
+          while(currLevelSize-- > 0)
+          {
+            TreeNode curr = q.poll();
+            currLevelValues.add(curr.val);
+
+            if(curr.left != null)
+            q.add(curr.left);
+
+            if(curr.right != null)
+            q.add(curr.right);
+          }
+
+          if(currLevel % 2  !=  0)
+          Collections.reverse(currLevelValues);
+
+          order.add(currLevelValues);
+
+             currLevel++;
+      }
+
+       
+       return order;
+
+    }
+}
+
+
+
+
+/*---------------------------------------------------------------------------------------------------*/
+
+
+
+
+// // Approach 1 : Using BFS + Stack !!!
+// // TC : O(V)
+// // Auxiliary SC : O(V/2) + O(V/2) [Since in case of Perfect Binary Tree Last Level has almost V/2 nodes so stack and queue can take max to max V/2 space each at a momement]
+
 // class Solution {
 //     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
          
@@ -72,47 +137,7 @@
 // }
 
 
-class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-         
-      if(root == null)
-      return new ArrayList<>();
 
-      List<List<Integer>> order = new ArrayList<>();
 
-      Queue<TreeNode> q = new ArrayDeque<>();
 
-      int currLevel = 0;
 
-      q.add(root);
-
-      while(!q.isEmpty())
-      {
-          int currLevelSize = q.size();
-          List<Integer> currLevelValues = new ArrayList<>();
-
-          while(currLevelSize-- > 0)
-          {
-            TreeNode curr = q.poll();
-            currLevelValues.add(curr.val);
-
-            if(curr.left != null)
-            q.add(curr.left);
-
-            if(curr.right != null)
-            q.add(curr.right);
-          }
-
-          if(currLevel % 2  != 0)
-          Collections.reverse(currLevelValues);
-
-          order.add(currLevelValues);
-
-             currLevel++;
-      }
-
-       
-       return order;
-
-    }
-}
