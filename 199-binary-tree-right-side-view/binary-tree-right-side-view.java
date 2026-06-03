61c83285057d4225cfae912a889866732f9a1f51
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-04 00:16:55 +0530

    Time: 2 ms (4.14%) | Memory: 43.7 MB (30.48%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,110 +14,70 @@
  * }
  */
 
-// class Pair{
+// Note : I have written two approaches for this problem below. Please read both approaches !!!
+
+
+
+
+// Approach 1 : Normal BFS + HashMap 
+// Hint :  Only the last node of every level is a part of right view !!!
+// TC : O(n)
+// Auxiliary SC : O(n)   [extra hashmap space + queue]
+
+class Pair{
  
-//   TreeNode curr;
-//   int lvl;
+  TreeNode curr;
+  int lvl;
 
-//   Pair(TreeNode curr,int lvl)
-//   {
-//     this.curr = curr;
-//     this.lvl = lvl;
-//   }
-// }
-
-// class Solution {
-    
-//     List<Integer> bfs(TreeNode root)
-//     {
-     
-//       HashMap<Integer,Integer> mp = new HashMap<>();
-
-//       Queue<Pair> q = new ArrayDeque<>();
-
-//       q.add(new Pair(root,0));
-      
-//       while(!q.isEmpty())
-//       {
-//          Pair top = q.poll();
-
-//          TreeNode curr = top.curr;
-//          int lvl = top.lvl;
-
-//          mp.put(lvl,curr.val);
-
-//          // explore the childrens
-
-//          if(curr.left != null)
-//          q.add(new Pair(curr.left,lvl+1));
-
-//          if(curr.right != null)
-//          q.add(new Pair(curr.right,lvl+1));
-//       }
-
-//       int maxLevel = Integer.MIN_VALUE;
-
-//       for(int lvl : mp.keySet())
-//       maxLevel = Math.max(maxLevel,lvl);
-
-//       List<Integer> rightView = new ArrayList<>();
-
-//       for(int level = 0;level<=maxLevel;level++)
-//       rightView.add(mp.get(level));
-
-
-//       return rightView;
-
-//     }
-//     public List<Integer> rightSideView(TreeNode root) {
-          
-//           if(root == null) return new ArrayList<>();
-
-//           return bfs(root);
-//     }
-// }
-
-
-
+  Pair(TreeNode curr,int lvl)
+  {
+    this.curr = curr;
+    this.lvl = lvl;
+  }
+}
 
 class Solution {
     
     List<Integer> bfs(TreeNode root)
     {
-      Queue<TreeNode> q = new ArrayDeque<>();
+     
+      HashMap<Integer,Integer> mp = new HashMap<>();
 
-      q.add(root);
+      Queue<Pair> q = new ArrayDeque<>();
 
-      List<Integer> rightView = new ArrayList<>();
+      q.add(new Pair(root,0));
       
       while(!q.isEmpty())
       {
-         int size = q.size();
+         Pair top = q.poll();
 
-         while(size-- > 1)
-         {
-            TreeNode curr = q.poll();
-            
-            if(curr.left != null)
-            q.add(curr.left);
+         TreeNode curr = top.curr;
+         int lvl = top.lvl;
 
-            if(curr.right != null)
-            q.add(curr.right);
-         }
+         mp.put(lvl,curr.val);
 
-         TreeNode curr = q.poll();  // last node of current Level
-         rightView.add(curr.val);
+         // explore the childrens
 
          if(curr.left != null)
-         q.add(curr.left);
+         q.add(new Pair(curr.left,lvl+1));
 
          if(curr.right != null)
-         q.add(curr.right);
+         q.add(new Pair(curr.right,lvl+1));
       }
+
+      int maxLevel = Integer.MIN_VALUE;
+
+      for(int lvl : mp.keySet())
+      maxLevel = Math.max(maxLevel,lvl);
+
+      List<Integer> rightView = new ArrayList<>();
+
+      for(int level = 0;level<=maxLevel;level++)
+      rightView.add(mp.get(level));
+
 
       return rightView;
 
-      
     }
     public List<Integer> rightSideView(TreeNode root) {
           
@@ -126,3 +86,64 @@ class Solution {
           return bfs(root);
     }
 }
+
+
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+// Approach 2 : Normal BFS
+// TC : O(n)
+// Auxiliary SC : O(n)   [queue space]
+
+// class Solution {
+    
+//     List<Integer> bfs(TreeNode root)
+//     {
+//       Queue<TreeNode> q = new ArrayDeque<>();
+
+//       q.add(root);
+
+//       List<Integer> rightView = new ArrayList<>();
+      
+//       while(!q.isEmpty())
+//       {
+//          int size = q.size();
+
+//          while(size-- > 1)
+//          {
+//             TreeNode curr = q.poll();
+            
+//             if(curr.left != null)
+//             q.add(curr.left);
+
+//             if(curr.right != null)
+//             q.add(curr.right);
+//          }
+
+//          TreeNode curr = q.poll();  // last node of current Level
+//          rightView.add(curr.val); // only last node of current level is a part of right view !!!
+
+//          if(curr.left != null)
+//          q.add(curr.left);
+
+//          if(curr.right != null)
+//          q.add(curr.right);
+//       }
+
+//       return rightView;
+
+      
+//     }
+//     public List<Integer> rightSideView(TreeNode root) {
+          
+//           if(root == null) return new ArrayList<>();
+
+//           return bfs(root);
+//     }
+// }
