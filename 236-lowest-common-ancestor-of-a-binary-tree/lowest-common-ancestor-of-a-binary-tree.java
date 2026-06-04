ad43a9d731a577b076feec13c9918ca3f31f72a0
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-04 17:30:18 +0530

    Time: 10 ms (9.83%) | Memory: 47.1 MB (5.42%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -7,6 +7,66 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// class Solution {
+
+//      void dfs(TreeNode root,TreeNode p,TreeNode q,List<TreeNode> pathSoFar,List<TreeNode> path1, List<TreeNode> path2)
+//      {
+//         if(root == null)
+//         return;
+
+//         if(!path1.isEmpty() && !path2.isEmpty()) return;
+
+//         pathSoFar.add(root);
+
+//         if(root == p)
+//         path1.addAll(pathSoFar);
+
+//         else if(root == q)
+//         path2.addAll(pathSoFar);
+
+//         dfs(root.left,p,q,pathSoFar,path1,path2);
+//         dfs(root.right,p,q,pathSoFar,path1,path2);
+
+//         pathSoFar.remove(pathSoFar.size()-1);
+//      }
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+           
+//           if(p == q)
+//           return p;
+
+//           List<TreeNode> path1 = new ArrayList<>();
+//           List<TreeNode> path2 = new ArrayList<>();
+
+//           dfs(root,p,q,new ArrayList<>(),path1,path2);
+
+//           Set<TreeNode> s = new HashSet<>();
+
+//           int n1 = path1.size();
+//           int n2 = path2.size();
+
+//           TreeNode lca = null;
+
+//           for(int i=n1-1;i>=0;i--)
+//           s.add(path1.get(i));
+
+//           for(int i=n2-1;i>=0;i--)
+//           {
+//              if(s.contains(path2.get(i)))
+//              {
+//              lca = path2.get(i);
+//              break;
+//              }
+//           }
+
+//           return lca;
+
+//     }
+// }
+
+
+
+
+
 class Solution {
 
      void dfs(TreeNode root,TreeNode p,TreeNode q,List<TreeNode> pathSoFar,List<TreeNode> path1, List<TreeNode> path2)
@@ -19,15 +79,10 @@ class Solution {
         pathSoFar.add(root);
 
         if(root == p)
-        {
-           for(TreeNode curr : pathSoFar)
-           path1.add(curr);
-        }
+        path1.addAll(pathSoFar);
+
         else if(root == q)
-        {
-           for(TreeNode curr : pathSoFar)
-            path2.add(curr);
-        }
+        path2.addAll(pathSoFar);
 
         dfs(root.left,p,q,pathSoFar,path1,path2);
         dfs(root.right,p,q,pathSoFar,path1,path2);
@@ -44,23 +99,17 @@ class Solution {
 
           dfs(root,p,q,new ArrayList<>(),path1,path2);
 
-          Set<TreeNode> mp = new HashSet<>();
-
           int n1 = path1.size();
           int n2 = path2.size();
 
           TreeNode lca = null;
 
-          for(int i=n1-1;i>=0;i--)
-          mp.add(path1.get(i));
+          int i=0;
 
-          for(int i=n2-1;i>=0;i--)
+          while(i<n1 && i<n2 && path1.get(i) == path2.get(i))
           {
-             if(mp.contains(path2.get(i)))
-             {
-             lca = path2.get(i);
-             break;
-             }
+             lca = path1.get(i);
+             i++;
           }
 
           return lca;
