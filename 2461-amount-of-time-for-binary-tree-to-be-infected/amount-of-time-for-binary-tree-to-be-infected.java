5678ed6ae6985013ef80d764c50bc89bba9b4802
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-07 11:53:56 +0530

    Time: 96 ms (56.57%) | Memory: 117.8 MB (55.28%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -14,100 +14,13 @@
  * }
  */
 
-// class Pair
-// {
-//    TreeNode curr;
-//    int lvl;
-
-//    Pair(TreeNode curr,int lvl)
-//    {
-//      this.curr = curr;
-//      this.lvl = lvl;
-//    }
-
-// }
-
-// class Solution {
-
-//     int bfs(TreeNode start, HashMap<TreeNode,TreeNode> parentMp)
-//     {
-//         int maxLvl = Integer.MIN_VALUE;
-        
-//         Queue<Pair> q = new ArrayDeque<>();
-//         HashSet<TreeNode> vis = new HashSet<>();
-
-//         q.add(new Pair(start,0));
-//         vis.add(start);
-
-//         while(!q.isEmpty())
-//         {
-//            Pair top = q.poll();
-
-//            TreeNode curr = top.curr;
-//            int lvl = top.lvl;
-
-//            maxLvl = Math.max(maxLvl,lvl);
-
-//            // explore the neighbours
-
-//            TreeNode parent = parentMp.get(curr);
-
-//            if(!vis.contains(parent) && parent != null)
-//            {
-//                 q.add(new Pair(parent,lvl+1));
-//                 vis.add(parent);
-//            }
-
-//            if(!vis.contains(curr.left) && curr.left != null)
-//            {
-//                 q.add(new Pair(curr.left,lvl+1));
-//                 vis.add(curr.left);
-//            }
-
-//            if(!vis.contains(curr.right) && curr.right != null)
-//            {
-//                q.add(new Pair(curr.right,lvl+1));
-//                vis.add(curr.right);
-//            }
-//         }
-
-//         return maxLvl;
-//     }
-   
-//     void dfs(TreeNode curr, TreeNode parent, HashMap<TreeNode,TreeNode> parentMp)
-//     {
-//        if(curr == null)
-//        return;
-
-//        parentMp.put(curr,parent);
-
-//        dfs(curr.left,curr,parentMp);
-//        dfs(curr.right,curr,parentMp);
-
-//     }
-
-//     public int amountOfTime(TreeNode root, int start) {
-         
-//           HashMap<TreeNode,TreeNode> parentMp = new HashMap<>();
-
-//           dfs(root,null,parentMp);
-
-//           TreeNode startNode = null;
-
-//           for(TreeNode curr : parentMp.keySet())
-//           {
-//              if(curr.val == start)
-//              {
-//                 startNode = curr;
-//                 break;
-//              }
-//           }
-
-//           return bfs(startNode,parentMp);
-//     }
-// }
+// Note : One traversal DFS approach left (not intuitive at all)
 
 
+// Approach 1 : Using DFS + BFS
+// Prerequisite Problem : https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/description/
+// TC : O(n)
+// SC : O(n)
 class Pair
 {
    TreeNode curr;
@@ -168,39 +81,37 @@ class Solution {
         return maxLvl;
     }
    
-    TreeNode dfs(TreeNode curr, TreeNode parent,int start, HashMap<TreeNode,TreeNode> parentMp)
+    void dfs(TreeNode curr, TreeNode parent, HashMap<TreeNode,TreeNode> parentMp)
     {
        if(curr == null)
-       return null;
+       return;
 
        parentMp.put(curr,parent);
 
-      TreeNode left =  dfs(curr.left,curr,start,parentMp);
-       TreeNode right = dfs(curr.right,curr,start,parentMp);
-        
-        if(curr.val == start)
-        return curr;
+       dfs(curr.left,curr,parentMp);
+       dfs(curr.right,curr,parentMp);
 
-        if(left == null && right == null)
-       {
-          if(curr.val == start)
-          return curr;
-          else 
-          return null;
-       }
-
-       if(left != null)
-       return left;
-
-       return right;
     }
 
     public int amountOfTime(TreeNode root, int start) {
          
           HashMap<TreeNode,TreeNode> parentMp = new HashMap<>();
 
-          TreeNode startNode = dfs(root,null,start,parentMp);
+          dfs(root,null,parentMp);
+
+          TreeNode startNode = null;
+
+          for(TreeNode curr : parentMp.keySet())
+          {
+             if(curr.val == start)
+             {
+                startNode = curr;
+                break;
+             }
+          }
 
           return bfs(startNode,parentMp);
     }
 }
+
+
