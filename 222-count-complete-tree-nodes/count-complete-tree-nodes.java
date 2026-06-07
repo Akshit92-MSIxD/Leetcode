66dbed0959b1c9d94940cb728f8042aceb567222
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-07 21:31:23 +0530

    Time: 0 ms (100.00%) | Memory: 49.3 MB (66.34%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,52 +14,43 @@
  * }
  */
 
-// class Solution {
-   
-//     int leftHeight(TreeNode root)
-//     {
-//         if(root == null)
-//         return 0;
-
-//         return 1 + leftHeight(root.left);
-//     }
-
-//      int rightHeight(TreeNode root)
-//      {
-//         if(root == null)
-//         return 0;
-
-//         return 1 + rightHeight(root.right);
-//      }
-
-
-//     public int countNodes(TreeNode root) {
-
-//          int lh = leftHeight(root);  // left height of root
-//          int rh = rightHeight(root);  // right height of root
-
-//          if(lh == rh)
-//             return (int)Math.pow(2,lh) - 1;
-
-//          int leftSubtree = countNodes(root.left);
-//          int rightSubtree = countNodes(root.right);
-
-//          return 1 + leftSubtree + rightSubtree;
-//     }
-// }
-
+// Approach : Using three seperate DFS functions
+// Note : Please consider height as no. of nodes not no. of edges !!!
+// TC : O(logn^2)
+// SC : O(logn) [since given tree is always complete tree]
 
 class Solution {
    
+    int leftHeight(TreeNode root)
+    {
+        if(root == null)
+        return 0;
+
+        return 1 + leftHeight(root.left);
+    }
+
+     int rightHeight(TreeNode root)
+     {
+        if(root == null)
+        return 0;
+
+        return 1 + rightHeight(root.right);
+     }
+
 
     public int countNodes(TreeNode root) {
-         
-         if(root == null)
-         return 0;
-         
+
+         int lh = leftHeight(root);  // left height of root
+         int rh = rightHeight(root);  // right height of root
+
+         if(lh == rh)  // condition to check if current tree starting from curr node is a perfect tree or node
+            return (int)Math.pow(2,lh) - 1;   // in case of perfect tree if h is given height -> total nodes -> 2^h - 1 (Note : Consider h as height in context of no. of nodes not no. of edges !!!)
+
          int leftSubtree = countNodes(root.left);
          int rightSubtree = countNodes(root.right);
 
          return 1 + leftSubtree + rightSubtree;
     }
 }
+
+
