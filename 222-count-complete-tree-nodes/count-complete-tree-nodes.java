6fd95486740f43813877ceffded781d59e366143
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-07 15:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 49.5 MB (47.10%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,33 +14,49 @@
  * }
  */
 
+// class Solution {
+   
+//     int leftHeight(TreeNode root)
+//     {
+//         if(root == null)
+//         return 0;
+
+//         return 1 + leftHeight(root.left);
+//     }
+
+//      int rightHeight(TreeNode root)
+//      {
+//         if(root == null)
+//         return 0;
+
+//         return 1 + rightHeight(root.right);
+//      }
+
+
+//     public int countNodes(TreeNode root) {
+
+//          int lh = leftHeight(root);  // left height of root
+//          int rh = rightHeight(root);  // right height of root
+
+//          if(lh == rh)
+//             return (int)Math.pow(2,lh) - 1;
+
+//          int leftSubtree = countNodes(root.left);
+//          int rightSubtree = countNodes(root.right);
+
+//          return 1 + leftSubtree + rightSubtree;
+//     }
+// }
+
+
 class Solution {
    
-    int leftHeight(TreeNode root)
-    {
-        if(root == null)
-        return 0;
-
-        return 1 + leftHeight(root.left);
-    }
-
-     int rightHeight(TreeNode root)
-     {
-        if(root == null)
-        return 0;
-
-        return 1 + rightHeight(root.right);
-     }
-
 
     public int countNodes(TreeNode root) {
-
-         int lh = leftHeight(root);  // left height of root
-         int rh = rightHeight(root);  // right height of root
-
-         if(lh == rh)
-            return (int)Math.pow(2,lh) - 1;
-
+         
+         if(root == null)
+         return 0;
+         
          int leftSubtree = countNodes(root.left);
          int rightSubtree = countNodes(root.right);
 
