e441ca82f58c8406a273e4c38fc7d7c903ecef74
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-10 17:12:25 +0530

    Time: 2 ms (62.56%) | Memory: 46.3 MB (62.54%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -53,43 +53,105 @@
 
 
 
+// // Approach 1 : Using Pure Recursive DFS(Expectation + Faith concept) !!! (Strivers Solution)
+// // Watch : https://www.youtube.com/watch?v=aZNaLrVebKQ&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=34
+
+// /* Worst case example [left skewed tree] :
+//  preorder = [1,2,3,4,5,6.......,n]  
+//  inorder  = [n,n-1,n-2,.....,3,2,1]  */
+
+// // TC : O(n^2) [ in case of left skewed tree] 
+// // SC : O(n) [in case of left skewed tree]
+
+// class Solution {
+
+
+//     TreeNode createBTree(int[] preorder,int preStart, int preEnd, int[] inorder, int inStart, int inEnd)
+//     {
+
+//         if(preStart > preEnd || inStart > inEnd)
+//         return null;
+
+//      TreeNode root = new TreeNode(preorder[preStart]);
+
+//      // find root in between inStart and inEnd in inorder[]
+
+//       int rootPos = -1;
+
+//       for(int i=inStart;i<=inEnd;i++)
+//       {
+//          if(inorder[i] == root.val)
+//          {
+//             rootPos = i;
+//             break;
+//          }
+//       }
+
+//       int leftLength = rootPos - inStart;    // left subtree inorder length
+     
+//       // create a new Preorder and Inorder range for left Subtree and return the root of rightSubtree
+//       root.left = createBTree(preorder,preStart+1,preStart+leftLength,inorder,inStart,rootPos - 1);
+
+//        // create a new Preorder and Inorder range for right Subtree and return the root of rightSubtree
+//       root.right = createBTree(preorder,preStart+leftLength+1,preEnd,inorder,rootPos+1,inEnd);
+
+//       return root;
+//     }
+
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+           
+//            return createBTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
+//     }
+// }
+
+
+
+// Approach 1 : Using Pure Recursive DFS(Expectation + Faith concept) !!! (Strivers Solution)
+// Watch : https://www.youtube.com/watch?v=aZNaLrVebKQ&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=34
+
+/* Worst case example [left skewed tree] :
+ preorder = [1,2,3,4,5,6.......,n]  
+ inorder  = [n,n-1,n-2,.....,3,2,1]  */
+
+// TC : O(n) [ even in case of left skewed tree] 
+// SC : O(n) [even in case of left skewed tree]
 
 class Solution {
-
+   
+     Map<Integer,Integer> indexMp = new HashMap<>();
 
     TreeNode createBTree(int[] preorder,int preStart, int preEnd, int[] inorder, int inStart, int inEnd)
     {
 
         if(preStart > preEnd || inStart > inEnd)
         return null;
-
+      
      TreeNode root = new TreeNode(preorder[preStart]);
 
-     // find root in between inStart and inEnd in inorder[]
+     // find root in between inStart and inEnd in inorder[] directly through indexMp
 
-      int rootPos = -1;
-
-      for(int i=inStart;i<=inEnd;i++)
-      {
-         if(inorder[i] == root.val)
-         {
-            rootPos = i;
-            break;
-         }
-      }
+      int rootPos = indexMp.get(root.val);
 
       int leftLength = rootPos - inStart;    // left subtree inorder length
-
+     
+      // create a new Preorder and Inorder range for left Subtree and return the root of rightSubtree
       root.left = createBTree(preorder,preStart+1,preStart+leftLength,inorder,inStart,rootPos - 1);
+
+       // create a new Preorder and Inorder range for right Subtree and return the root of rightSubtree
       root.right = createBTree(preorder,preStart+leftLength+1,preEnd,inorder,rootPos+1,inEnd);
 
       return root;
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+           for(int i=0;i<inorder.length;i++)
+           indexMp.put(inorder[i],i);
            
            return createBTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
     }
 }
+
+
 
 
