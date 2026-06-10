efbd203c3fb87f8e58d280afac7b135c63f23741
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-10 14:08:40 +0530

    Time: 3 ms (29.86%) | Memory: 45.3 MB (88.52%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,41 +13,82 @@
  *     }
  * }
  */
- // Approach : DFS
- // Watch : https://www.youtube.com/watch?v=G5c1wM3Kpuw (for better understanding)
+//  // Approach : DFS
+//  // Watch : https://www.youtube.com/watch?v=G5c1wM3Kpuw (for better understanding)
+
+// class Solution {
+//     int idx = 0 ; // it is created to iterate over preorder[] only 
+
+//     TreeNode createBTree(int s, int e, int[] inorder, int[] preorder)
+//     {
+//         if(s > e)
+//         return null;
+
+//         TreeNode root = new TreeNode(preorder[idx]);
+//         idx++;
+        
+//         int rootPos = -1;
+
+//         for(int i=s;i<=e;i++)
+//         {
+//             if(inorder[i] == root.val)
+//             {
+//                 rootPos = i;
+//                 break;
+//             }
+
+//         }
+
+//         root.left = createBTree(s,rootPos-1,inorder,preorder);
+//         root.right = createBTree(rootPos+1,e,inorder,preorder);
+
+//         return root;
+//     }
+
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+           
+//            return createBTree(0,inorder.length-1,inorder,preorder);
+//     }
+// }
+
+
+
 
 class Solution {
-    int idx = 0 ; // it is created to iterate over preorder[] only 
 
-    TreeNode createBTree(int s, int e, int[] inorder, int[] preorder)
+
+    TreeNode createBTree(int[] preorder,int preStart, int preEnd, int[] inorder, int inStart, int inEnd)
     {
-        if(s > e)
+
+        if(preStart > preEnd || inStart > inEnd)
         return null;
 
-        TreeNode root = new TreeNode(preorder[idx]);
-        idx++;
-        
-        int rootPos = -1;
+     TreeNode root = new TreeNode(preorder[preStart]);
 
-        for(int i=s;i<=e;i++)
-        {
-            if(inorder[i] == root.val)
-            {
-                rootPos = i;
-                break;
-            }
+     // find root in between inStart and inEnd in inorder[]
 
-        }
+      int rootPos = -1;
 
-        root.left = createBTree(s,rootPos-1,inorder,preorder);
-        root.right = createBTree(rootPos+1,e,inorder,preorder);
+      for(int i=inStart;i<=inEnd;i++)
+      {
+         if(inorder[i] == root.val)
+         {
+            rootPos = i;
+            break;
+         }
+      }
 
-        return root;
+      int leftLength = rootPos - inStart;    // left subtree inorder length
+
+      root.left = createBTree(preorder,preStart+1,preStart+leftLength,inorder,inStart,rootPos - 1);
+      root.right = createBTree(preorder,preStart+leftLength+1,preEnd,inorder,rootPos+1,inEnd);
+
+      return root;
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
            
-           return createBTree(0,inorder.length-1,inorder,preorder);
+           return createBTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
     }
 }
 
