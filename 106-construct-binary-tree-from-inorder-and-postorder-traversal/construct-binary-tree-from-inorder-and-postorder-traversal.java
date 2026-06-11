01dc755aa4e9aa6773c0a887d014a3969cdac5e9
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-11 13:33:12 +0530

    Time: 1 ms (98.81%) | Memory: 46 MB (72.40%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -13,7 +13,82 @@
  *     }
  * }
  */
+
+// // Approach 1 : Using Pure Recursive DFS(Expectation + Faith concept) !!! 
+
+// // Prerequisite Problem (Must!!!) : https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+// // Note : In this approach always traverse postorder from reverse-order/R-to-L and inorder L-to-R(as usual) !!!
+
+// /* Worst case example [left skewed tree] :
+//  preorder = [n,n-1,n-2,.......,3,2,1]  
+//  inorder  = [n,n-1,n-2,.....,3,2,1]  */
+
+// // TC : O(n^2) [ in case of left skewed tree] 
+// // SC : O(n) [in case of left skewed tree]
+
+
+// class Solution {
+    
+//     TreeNode createBTree(int[] postOrder, int postStart, int postEnd, int[] inOrder, int inStart, int inEnd)
+//     {
+//          if(postEnd>postStart || inStart>inEnd)
+//          return null;
+
+//          TreeNode root = new TreeNode(postOrder[postStart]);
+
+//          int rootPos = -1;
+
+//          for(int i=inStart;i<=inEnd;i++)
+//          {
+//             if(inOrder[i] == root.val)
+//             {
+//             rootPos = i;
+//             break;
+//             }
+//          }
+
+//          int rightLength = inEnd - rootPos;
+
+//          root.right = createBTree(postOrder,postStart-1,postStart-rightLength,inOrder,rootPos+1,inEnd);
+//          root.left = createBTree(postOrder,postStart-rightLength-1,postEnd,inOrder,inStart,rootPos-1);
+
+//          return root;  
+//     }
+
+//     public TreeNode buildTree(int[] inOrder, int[] postOrder) {
+         
+//          return createBTree(postOrder,postOrder.length-1,0,inOrder,0,inOrder.length-1);
+//     }
+// }
+
+
+
+
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+// Approach 2 : Using Pure Recursive DFS(Expectation + Faith concept) + HashMap !!! 
+
+// Prerequisite Problem (Must!!!) : https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+// Note : In this approach always traverse postorder from reverse-order/R-to-L and inorder L-to-R(as usual) !!!
+
+/* Worst case example [left skewed tree] :
+ preorder = [n,n-1,n-2,.......,3,2,1]  
+ inorder  = [n,n-1,n-2,.....,3,2,1]  */
+
+// TC : O(n^2) [ in case of left skewed tree] 
+// SC : O(n) [in case of left skewed tree]
+
+
 class Solution {
+
+    HashMap<Integer,Integer> indexMp = new HashMap<>();
     
     TreeNode createBTree(int[] postOrder, int postStart, int postEnd, int[] inOrder, int inStart, int inEnd)
     {
@@ -22,16 +97,7 @@ class Solution {
 
          TreeNode root = new TreeNode(postOrder[postStart]);
 
-         int rootPos = -1;
-
-         for(int i=inStart;i<=inEnd;i++)
-         {
-            if(inOrder[i] == root.val)
-            {
-            rootPos = i;
-            break;
-            }
-         }
+         int rootPos = indexMp.get(root.val);
 
          int rightLength = inEnd - rootPos;
 
@@ -42,6 +108,10 @@ class Solution {
     }
 
     public TreeNode buildTree(int[] inOrder, int[] postOrder) {
+
+         for(int i=0;i<inOrder.length;i++)
+          indexMp.put(inOrder[i],i);
+
          
          return createBTree(postOrder,postOrder.length-1,0,inOrder,0,inOrder.length-1);
     }
