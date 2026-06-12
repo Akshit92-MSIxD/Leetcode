6d75214b2646d858980b9fc412b52a8c695b47ce
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-12 13:09:36 +0530

    Time: 17 ms (25.43%) | Memory: 48.2 MB (38.01%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -7,85 +7,183 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// public class Codec {
+
+//     // Encodes a tree to a single string.
+//     public String serialize(TreeNode root) {
+         
+//          if(root == null)
+//          return "";
+
+//         Queue<TreeNode> q = new LinkedList<>();
+
+//         q.add(root); 
+
+//         StringBuilder res = new StringBuilder();
+
+//         while(!q.isEmpty())
+//         {
+//             TreeNode curr = q.poll();
+            
+//             if(curr == null)
+//             res.append("#");
+//             else
+//             res.append(curr.val);
+     
+//               res.append(",");
+
+//               if(curr != null)
+//               {
+//                  q.add(curr.left);
+//                  q.add(curr.right);
+//               }
+//         }
+
+//           res.setLength(res.length()-1); // remove the last trailing commas
+
+//           return res.toString();
+
+//     }
+
+//     // Decodes your encoded data to tree.
+//     public TreeNode deserialize(String data) {
+
+//         if(data.isEmpty())
+//         return null;
+         
+//           String[] res = data.split(",");
+
+//           TreeNode root = new TreeNode(Integer.parseInt(res[0]));
+
+//           Queue<TreeNode> q = new ArrayDeque<>();
+
+//           q.add(root);
+
+//           int i = 1;
+
+//           while(!q.isEmpty() && i<res.length)
+//           {
+//               TreeNode curr = q.poll();
+
+//               if(!res[i].equals("#"))
+//               {
+//                 curr.left = new TreeNode(Integer.parseInt(res[i]));
+//                 q.add(curr.left);
+//               }
+               
+//                i++;
+
+//               if(!res[i].equals("#"))
+//               {
+//                 curr.right = new TreeNode(Integer.parseInt(res[i]));
+//                 q.add(curr.right);
+//               }
+
+//               i++;
+//           }
+
+//           return root;
+
+//     }
+// }
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------*/
+
+
+class Pair{
+
+    TreeNode curr;
+    int state;
+
+    Pair(TreeNode curr, int state)
+    {
+        this.curr = curr;
+        this.state = state;
+    }
+} 
+
 public class Codec {
+
+    void dfs(TreeNode root, StringBuilder preOrder)
+    {
+         if(root == null)
+         {
+         preOrder.append("#,");
+         return;
+         }
+
+         preOrder.append(root.val+",");
+
+         dfs(root.left,preOrder);
+         dfs(root.right,preOrder);
+    }
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-         
+
          if(root == null)
          return "";
+         
+         StringBuilder preOrder = new StringBuilder();
 
-        Queue<TreeNode> q = new LinkedList<>();
+         dfs(root,preOrder);
 
-        q.add(root); 
+         preOrder.setLength(preOrder.length()-1);
 
-        StringBuilder res = new StringBuilder();
-
-        while(!q.isEmpty())
-        {
-            TreeNode curr = q.poll();
-            
-            if(curr == null)
-            res.append("#");
-            else
-            res.append(curr.val);
-     
-              res.append(",");
-
-              if(curr != null)
-              {
-                 q.add(curr.left);
-                 q.add(curr.right);
-              }
-        }
-
-          res.setLength(res.length()-1); // remove the last trailing commas
-
-          return res.toString();
-
+         return preOrder.toString();
+          
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-
+        
         if(data.isEmpty())
         return null;
-         
-          String[] res = data.split(",");
 
-          TreeNode root = new TreeNode(Integer.parseInt(res[0]));
+        String[] preOrder = data.split(",");
 
-          Queue<TreeNode> q = new ArrayDeque<>();
+        Stack<Pair> st = new Stack<>();
 
-          q.add(root);
+        TreeNode root = new TreeNode(Integer.parseInt(preOrder[0]));
 
-          int i = 1;
+        st.push(new Pair(root,0));
 
-          while(!q.isEmpty() && i<res.length)
-          {
-              TreeNode curr = q.poll();
+        int i = 1;
 
-              if(!res[i].equals("#"))
-              {
-                curr.left = new TreeNode(Integer.parseInt(res[i]));
-                q.add(curr.left);
-              }
-               
-               i++;
+        while(!st.empty())
+        {
 
-              if(!res[i].equals("#"))
-              {
-                curr.right = new TreeNode(Integer.parseInt(res[i]));
-                q.add(curr.right);
-              }
+            TreeNode newNode = null;
 
-              i++;
-          }
+            if(!preOrder[i].equals("#"))
+            newNode = new TreeNode(Integer.parseInt(preOrder[i]));
 
-          return root;
+            if(st.peek().state == 0)
+            {
+                st.peek().curr.left = newNode;
+                st.peek().state += 1;
+            }
+            else
+            {
+                 st.peek().curr.right = newNode;
+                 st.pop();
+            }
+             
+             if(newNode != null)
+             st.push(new Pair(newNode,0));
 
+           i++;
+        }
+
+        return root;
+ 
     }
 }
+
+
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
