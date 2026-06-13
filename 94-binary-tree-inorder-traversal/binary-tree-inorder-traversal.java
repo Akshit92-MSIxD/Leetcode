f56005e6cc4667741abd5db104edf6532f2b2cab
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-13 12:38:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (69.05%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -15,81 +15,84 @@
  */
 
 
-// class Solution {
-//     public List<Integer> inorderTraversal(TreeNode root) {
-          
-//           if(root == null)
-//           return new ArrayList<>();
-
-//           List<Integer> inorder = new ArrayList<>();
-
-//           TreeNode curr = root;
-
-//           while(curr != null)
-//           {
-//              TreeNode temp = curr.left;
-
-//              if(temp == null)
-//              {
-//                 inorder.add(curr.val);
-//                 curr = curr.right;
-//                 continue;
-//              }
-
-//              while(temp.right != null)
-//              temp = temp.right;
-
-//              temp.right = curr;
-
-//              TreeNode nextNode = curr.left;
-
-//              curr.left = null;
-
-//              curr = nextNode;
-//           }
-
-//           return inorder;
-//     }
-// }
-
-
-
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-           
-        List<Integer> inOrder = new ArrayList<>();
-
-        TreeNode curr = root;
-
-        while(curr != null)
-        {
-            if(curr.left == null)
-            {
-                inOrder.add(curr.val);
-                curr = curr.right;
-            }
-            else
-            {
-                TreeNode pred = curr.left;   // inorder predecessor of curr 
-
-                while(pred.right != null && pred.right != curr)
-                pred = pred.right;
-
-                if(pred.right == null)
-                {
-                pred.right = curr;
-                curr = curr.left;
-                }
-                else
-                {
-                    pred.right = null;
-                    inOrder.add(curr.val);
-                    curr = curr.right;
-                }
-            }
-        }
-
-        return inOrder;
           
+          if(root == null)
+          return new ArrayList<>();
+
+          List<Integer> inorder = new ArrayList<>();
+
+          TreeNode curr = root;
+
+          while(curr != null)
+          {
+
+             if(curr.left == null)
+             {
+                inorder.add(curr.val);
+                curr = curr.right;
+             }
+             else
+             {
+              
+              TreeNode pred = curr.left;
+
+             while(pred.right != null)
+             pred = pred.right;
+
+             pred.right = curr; // create a thread or link
+
+             TreeNode leftChildren = curr.left;
+
+             curr.left = null; // breaking the edge or connection between curr and its left children once curr moves to its left children
+
+             curr = leftChildren;
+             }
+
     }
+          return inorder;
 }
+}
+
+
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+           
+//         List<Integer> inOrder = new ArrayList<>();
+
+//         TreeNode curr = root;
+
+//         while(curr != null)
+//         {
+//             if(curr.left == null)
+//             {
+//                 inOrder.add(curr.val);
+//                 curr = curr.right;
+//             }
+//             else
+//             {
+//                 TreeNode pred = curr.left;   // inorder predecessor of curr 
+
+//                 while(pred.right != null && pred.right != curr) 
+//                 pred = pred.right;
+
+//                 if(pred.right == null) // create a thread or link if this condition is true !!!
+//                 {
+//                 pred.right = curr;
+//                 curr = curr.left;
+//                 }
+//                 else                // break the thread or link in case this condition is true !!!
+//                 {
+//                     pred.right = null;
+//                     inOrder.add(curr.val);
+//                     curr = curr.right;
+//                 }
+//             }
+//         }
+
+//         return inOrder;
+          
+//     }
+// }
