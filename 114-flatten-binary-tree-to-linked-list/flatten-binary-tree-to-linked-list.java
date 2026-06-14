cedd7b8a0105efe4877968b47a342716b2de3464
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-14 14:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (32.22%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -13,40 +13,58 @@
  *     }
  * }
  */
-class Solution {
+// class Solution {
 
-    TreeNode dfs(TreeNode root)
-    {    
-          if(root == null)
-          return null;
+//     TreeNode dfs(TreeNode root)
+//     {    
+//           if(root == null)
+//           return null;
 
-          if(root.left == null && root.right == null)
-          return root;
+//           if(root.left == null && root.right == null)
+//           return root;
 
-          
-          TreeNode leftBottom = dfs(root.left);
-          TreeNode rightBottom = dfs(root.right);
+//           TreeNode leftBottom = dfs(root.left);
+//           TreeNode rightBottom = dfs(root.right);
 
-          if(leftBottom == null)
-          return rightBottom;
+//           if(leftBottom == null)
+//           return rightBottom;
 
-          if(rightBottom == null)
-          {
-            root.right = root.left;
-            root.left = null;
-            return leftBottom;
-          }
+//           if(rightBottom == null)
+//           {
+//             root.right = root.left;
+//             root.left = null;
+//             return leftBottom;
+//           }
 
-          leftBottom.right = root.right;
-          TreeNode temp = root.left;
-          root.left = null;
-          root.right = temp;
+//           leftBottom.right = root.right;
+//           TreeNode temp = root.left;
+//           root.left = null;
+//           root.right = temp;
 
-          return rightBottom;
-    }
+//           return rightBottom;
+//     }
 
-    public void flatten(TreeNode root) {
+//     public void flatten(TreeNode root) {
            
-            dfs(root);
+//             dfs(root);
+//     }
+// }
+
+
+
+class Solution {
+     
+     TreeNode prev = null;
+    public void flatten(TreeNode root) {
+            
+        if(root == null)
+        return;
+
+        flatten(root.right);
+        flatten(root.left);
+
+        root.right = prev;
+        root.left = null;
+        prev = root;
     }
 }
