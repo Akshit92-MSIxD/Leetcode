db17a99ebf7a85965a9316365e67f9fc3525fcdf
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-17 01:30:31 +0530

    Time: 72 ms (76.62%) | Memory: 164.1 MB (19.79%) - LeetSync

--- 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.java
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.java
@@ -88,64 +88,102 @@
 
 
 
+// class Solution {
+
+//     int getFloorValue(List<Integer> inOrder, int k)
+//     { 
+//          int low = 0;
+//          int high = inOrder.size()-1;
+
+//          int floor = Integer.MIN_VALUE;
+
+//          while(low<=high)
+//          {
+//             int mid = low + (high - low)/2;
+
+//              if(inOrder.get(mid) <= k)
+//              {
+//                  floor = inOrder.get(mid);
+//                  low = mid + 1;
+//              }
+//              else
+//              {
+//                 high = mid - 1;
+//              } 
+
+//          }
+
+//          if(floor == Integer.MIN_VALUE)
+//          floor = -1;
+
+//          return floor;
+//     }
+
+//     int getCeilValue(List<Integer> inOrder, int k)
+//     {
+//            int low = 0;
+//            int high = inOrder.size() - 1;
+
+//            int ceil = Integer.MAX_VALUE;
+
+//            while(low<=high)
+//            {
+//               int mid = low + (high - low)/2;
+
+//               if(inOrder.get(mid) >= k)
+//               {
+//                  ceil = inOrder.get(mid);
+//                  high = mid - 1;
+//               }
+//               else
+//               {
+//                 low = mid + 1;
+//               }
+//            }
+
+//            if(ceil == Integer.MAX_VALUE)
+//            ceil = -1;
+
+//            return ceil;
+//     }
+
+//     void dfs(TreeNode root,List<Integer> inOrder)
+//     {
+//         if(root == null)
+//         return;
+
+//         dfs(root.left,inOrder);
+//         inOrder.add(root.val);
+//         dfs(root.right,inOrder);
+//     }
+
+//     public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
+               
+//              List<List<Integer>> ans = new ArrayList<>();
+              
+//               List<Integer> inOrder = new ArrayList<>();  // inOrder of BST give nodes in sorted order by their values !!!
+
+//               dfs(root,inOrder);  
+
+//              for(int k : queries)
+//              {
+//                  int floor = getFloorValue(inOrder,k);
+//                  int ceil =  getCeilValue(inOrder,k);
+
+//                  List<Integer> subans = new ArrayList<>();
+//                  subans.add(floor);
+//                  subans.add(ceil);
+
+//                  ans.add(subans);
+//              }
+           
+//            return ans;
+
+//     }
+// }
+
+
 class Solution {
-
-    int getFloorValue(List<Integer> inOrder, int k)
-    { 
-         int low = 0;
-         int high = inOrder.size()-1;
-
-         int floor = Integer.MIN_VALUE;
-
-         while(low<=high)
-         {
-            int mid = low + (high - low)/2;
-
-             if(inOrder.get(mid) <= k)
-             {
-                 floor = inOrder.get(mid);
-                 low = mid + 1;
-             }
-             else
-             {
-                high = mid - 1;
-             } 
-
-         }
-
-         if(floor == Integer.MIN_VALUE)
-         floor = -1;
-
-         return floor;
-    }
-
-    int getCeilValue(List<Integer> inOrder, int k)
-    {
-           int low = 0;
-           int high = inOrder.size() - 1;
-
-           int ceil = Integer.MAX_VALUE;
-
-           while(low<=high)
-           {
-              int mid = low + (high - low)/2;
-
-              if(inOrder.get(mid) >= k)
-              {
-                 ceil = inOrder.get(mid);
-                 high = mid - 1;
-              }
-              else
-              {
-                low = mid + 1;
-              }
-           }
-
-           if(ceil == Integer.MAX_VALUE)
-           ceil = -1;
-
-           return ceil;
-    }
 
     void dfs(TreeNode root,List<Integer> inOrder)
     {
@@ -161,20 +199,51 @@ class Solution {
                
              List<List<Integer>> ans = new ArrayList<>();
               
-              List<Integer> inOrder = new ArrayList<>();  // inOrder of BST give nodes in sorted order by their values !!!
+             List<Integer> inOrder = new ArrayList<>();  // inOrder of BST give nodes in sorted order by their values !!!
 
               dfs(root,inOrder);  
 
              for(int k : queries)
              {
-                 int floor = getFloorValue(inOrder,k);
-                 int ceil =  getCeilValue(inOrder,k);
+                 int floor = Integer.MIN_VALUE;
+                 int ceil =  Integer.MAX_VALUE;
 
-                 List<Integer> subans = new ArrayList<>();
-                 subans.add(floor);
-                 subans.add(ceil);
+                 int low = 0;
+                 int high = inOrder.size() - 1;
 
-                 ans.add(subans);
+                    while(low<=high)
+                    {
+                        int mid = low + (high - low)/2;
+
+                        if(inOrder.get(mid) > k)
+                        {
+                            ceil = inOrder.get(mid);
+                            high = mid - 1;
+                        }
+                        else if(inOrder.get(mid) < k)
+                        {
+                            floor = inOrder.get(mid);
+                            low = mid + 1;
+                        }
+                        else
+                        {
+                            floor = ceil = k;
+                            break;
+                        }
+                    }
+
+                        if(floor == Integer.MIN_VALUE)
+                        floor = -1;
+
+                        if(ceil == Integer.MAX_VALUE)
+                        ceil = -1;
+
+
+                        List<Integer> subans = new ArrayList<>();
+                        subans.add(floor);
+                        subans.add(ceil);
+
+                        ans.add(subans);
              }
            
            return ans;
