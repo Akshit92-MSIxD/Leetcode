9be6556236a7702dd53b61f4792b659f90193581
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-18 13:25:04 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (28.73%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -13,56 +13,76 @@
  *     }
  * }
  */
-// class Solution {
-//     public TreeNode insertIntoBST(TreeNode root, int val) {
 
-//             if(root == null)
-//             return new TreeNode(val);
-            
-//              TreeNode curr = root;
-//              TreeNode prev = null;
+// Note : I have written two approaches below . Please read both of them !!!
+// **Note : I have written one more brute force unique approach in C++ section of this problem. Please read that also !!!
 
-//              while(curr != null)
-//              {   
 
-//                  if(val>curr.val)
-//                  {
-//                     prev = curr;
-//                     curr = curr.right;
-//                  }
-//                  else
-//                  {
-//                    prev = curr;
-//                    curr = curr.left;
-//                  }
-//              }
 
-//              if(val < prev.val)
-//                 prev.left = new TreeNode(val);
-//              else
-//                prev.right = new TreeNode(val); 
-
-//             return root;
-
-//     }
-// }
-
+// Approach 1 : Pure Iterative Curr Approach !!!
+// TC : O(n) [in case of degenerate BST tree]
+// SC : O(1)
 
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
 
-           if(root == null)
-           return new TreeNode(val);
+            if(root == null)
+            return new TreeNode(val);
+            
+             TreeNode curr = root;
+             TreeNode prev = null;
 
+             while(curr != null)
+             {   
 
-           if(val > root.val)
-             root.right = insertIntoBST(root.right,val);
-           else
-             root.left = insertIntoBST(root.left,val);
+                 if(val>curr.val)
+                 {
+                    prev = curr;
+                    curr = curr.right;
+                 }
+                 else
+                 {
+                   prev = curr;
+                   curr = curr.left;
+                 }
+             }
 
-             return root;
+             if(val < prev.val)
+                prev.left = new TreeNode(val);
+             else
+               prev.right = new TreeNode(val); 
+
+            return root;
 
     }
 }
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------*/
+
+
+
+// Approach 2 : Pure Recursive Expectation + Faith Approach
+// TC : O(n) [in case of degenerate tree]
+// SC : O(n) [in case of degenerate tree]
+
+// class Solution {
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+
+//            if(root == null)
+//            return new TreeNode(val);
+
+
+//            if(val > root.val)
+//              root.right = insertIntoBST(root.right,val);
+//            else
+//              root.left = insertIntoBST(root.left,val);
+
+//              return root;
+
+//     }
+// }
 
 
