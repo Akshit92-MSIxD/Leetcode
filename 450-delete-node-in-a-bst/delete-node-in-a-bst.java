d14c8f17c4aab5afbe56281626f1b7edcdc7efa8
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-19 13:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (35.94%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -13,111 +13,145 @@
  *     }
  * }
  */
+
+ // Note : I have written two approaches for this problem below . Please read both of them !!!
+ // *** Note : First Read Approach1  then Approach 2 !!!
+
+
+
+// Approach 1 : Recursive Expectation and Faith Approach
+// Hint : Try Attaching RightSubtree to the rightmost node of leftSubTree(if leftSubtree exits !!!)
+// TC : O(n) [in case of degenerate BST]
+// SC : O(n) [ in case of generate BST]
+
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
             
-            if(root == null)
-            return null;
-
-            TreeNode keyNode = null;
-            
-            TreeNode prev = null;
-            TreeNode curr = root;
-
-            while(curr != null)
-            {
-                if(key == curr.val)
-                {
-                    keyNode = curr;
-                    break;
-                }
-                else if(key < curr.val)
-                {
-                    prev = curr;
-                    curr = curr.left;
-                }
-                else
-                {
-                    prev = curr;
-                    curr = curr.right;
-                }
-            }
-
-            if(keyNode == null)
-            return root;
+          if(root == null)
+          return null;
 
 
-            TreeNode leftPart = keyNode.left;
-            TreeNode rightPart = keyNode.right;
+          if(root.val == key)
+          {
+             TreeNode leftSubtree = root.left;
+             TreeNode rightSubtree = root.right;
 
-            curr = leftPart;
+             TreeNode curr = leftSubtree;
 
-            while(curr != null && curr.right != null)
-            curr = curr.right;
+             while(curr != null && curr.right != null)
+             curr = curr.right;
 
-            if(curr != null)
-            curr.right = rightPart;
+             if(curr != null)
+             curr.right = rightSubtree;
 
+             root.left = root.right = null;
 
-            if(prev == null)
-            {
-                keyNode.left = keyNode.right = null;
-                return (leftPart == null) ? rightPart : leftPart;
-            }
+             return (leftSubtree == null) ? rightSubtree : leftSubtree;
+          }
+          else if(root.val > key)
+          {
+            root.left = deleteNode(root.left,key);
+          }
+          else
+          {
+            root.right = deleteNode(root.right,key);
+          }
 
+          return root;
 
-            if(prev.left == keyNode)
-            {
-                keyNode.left = keyNode.right = null;
-                prev.left = (leftPart == null) ? rightPart : leftPart;
-            }
-            else
-            {
-                keyNode.right = keyNode.left = null;
-                prev.right = (leftPart == null) ? rightPart : leftPart;
-            }
-
-            return root;
     }
 }
 
 
 
 
+
+/*-----------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+// Approach 2 : Iterative Curr + Parent(Prev) Approach
+// Hint : Try Attaching RightSubtree to the rightmost node of leftSubTree(if leftSubtree exits !!!)
+// TC : O(n) [in case of degenerate BST]
+// SC : O(1) [no extra space]
+
+
 // class Solution {
 //     public TreeNode deleteNode(TreeNode root, int key) {
             
-//           if(root == null)
-//           return null;
+//             if(root == null)
+//             return null;
+
+//             TreeNode keyNode = null;
+            
+//             TreeNode parent = null;
+//             TreeNode curr = root;
+
+//             while(curr != null)
+//             {
+//                 if(key == curr.val)
+//                 {
+//                     keyNode = curr;
+//                     break;
+//                 }
+//                 else if(key < curr.val)
+//                 {
+//                     parent = curr;
+//                     curr = curr.left;
+//                 }
+//                 else
+//                 {
+//                     parent = curr;
+//                     curr = curr.right;
+//                 }
+//             }
+
+//             if(keyNode == null)
+//             return root;
 
 
-//           if(root.val == key)
-//           {
-//              TreeNode leftPart = root.left;
-//              TreeNode rightPart = root.right;
+//             TreeNode leftSubtree = keyNode.left;
+//             TreeNode rightSubtree = keyNode.right;
 
-//              TreeNode curr = leftPart;
+//             curr = leftSubtree;
 
-//              while(curr != null && curr.right != null)
-//              curr = curr.right;
+//             while(curr != null && curr.right != null)
+//             curr = curr.right;
 
-//              if(curr != null)
-//              curr.right = rightPart;
+//             if(curr != null)
+//             curr.right = rightSubtree;
 
-//              root.left = root.right = null;
 
-//              return (leftPart == null) ? rightPart : leftPart;
-//           }
-//           else if(root.val > key)
-//           {
-//             root.left = deleteNode(root.left,key);
-//           }
-//           else
-//           {
-//             root.right = deleteNode(root.right,key);
-//           }
+//             if(parent == null)
+//             {
+//                 keyNode.left = keyNode.right = null;
+//                 return (leftSubtree == null) ? rightSubtree : leftSubtree;
+//             }
 
-//           return root;
 
+//             if(parent.left == keyNode)
+//             {
+//                 keyNode.left = keyNode.right = null;
+//                 parent.left = (leftSubtree == null) ? rightSubtree : leftSubtree;
+//             }
+//             else
+//             {
+//                 keyNode.right = keyNode.left = null;
+//                 parent.right = (leftSubtree == null) ? rightSubtree : leftSubtree;
+//             }
+
+//             return root;
 //     }
 // }
+
+
+
+
+
+
+
+
+
