039660a4f63ae85e1074b099a5a0ae437f8b0343
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-20 23:17:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (57.83%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -14,46 +14,81 @@
 //  * }
 //  */
 
+
+// Approach 1 : Creating BST from Preorder and Inorder !!!
+// Hint : To find the inorder --> just sort the preorder since we already have preorder of a BST
+// TC : O(nlogn) [sorting preorder to find inorder] + O(n) [recursive DFS to create BST]
+// SC : O(n) [recursive stack space i.e height of tree in case of degenerate BST] + O(n) [extra inorder[] array]
+
+// class Solution {
+
+//     TreeNode bstFromPreorderAndInorder(int[] preorder,int preStart, int preEnd, int[] inorder, int inStart, int inEnd)
+//     {
+//            if(preStart > preEnd || inStart > inEnd)
+//            return null;
+
+//            TreeNode root = new TreeNode(preorder[preStart]);
+
+//            int rootPos = -1;
+
+//            for(int i=inStart;i<=inEnd;i++)
+//            {
+//              if(inorder[i] == root.val)
+//              {
+//                 rootPos = i;
+//                 break;
+//              }
+//            }
+
+//            int leftLength = rootPos - inStart;
+
+//            root.left = bstFromPreorderAndInorder(preorder,preStart+1,preStart+leftLength,inorder,inStart,rootPos-1);
+//            root.right = bstFromPreorderAndInorder(preorder,preStart+leftLength+1,preEnd,inorder,rootPos+1,inEnd);
+
+//            return root;
+//     }
+
+
+
+//     public TreeNode bstFromPreorder(int[] preorder) {
+           
+//             int[] inorder = new int[preorder.length];
+
+//             for(int i=0;i<inorder.length;i++)
+//              inorder[i] = preorder[i];
+
+//              Arrays.sort(inorder);
+
+//              return bstFromPreorderAndInorder(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
+
+//     }
+// }
+
+
+
+/*-------------------------------------------------------------------------------------------------------------*/
+
+
+
 class Solution {
+   
+    int idx = 0;
 
-    TreeNode bstFromPreorderAndInorder(int[] preorder,int preStart, int preEnd, int[] inorder, int inStart, int inEnd)
+    TreeNode createBST(int[] preorder, int upperBound)
     {
-           if(preStart > preEnd || inStart > inEnd)
-           return null;
+          if(idx == preorder.length ||  preorder[idx] > upperBound)
+          return null;
 
-           TreeNode root = new TreeNode(preorder[preStart]);
+          TreeNode root = new TreeNode(preorder[idx++]);
 
-           int rootPos = -1;
+          root.left = createBST(preorder,root.val);
+          root.right = createBST(preorder,upperBound);
 
-           for(int i=inStart;i<=inEnd;i++)
-           {
-             if(inorder[i] == root.val)
-             {
-                rootPos = i;
-                break;
-             }
-           }
-
-           int leftLength = rootPos - inStart;
-
-           root.left = bstFromPreorderAndInorder(preorder,preStart+1,preStart+leftLength,inorder,inStart,rootPos-1);
-           root.right = bstFromPreorderAndInorder(preorder,preStart+leftLength+1,preEnd,inorder,rootPos+1,inEnd);
-
-           return root;
+          return root;
     }
-
-
-
     public TreeNode bstFromPreorder(int[] preorder) {
            
-            int[] inorder = new int[preorder.length];
-
-            for(int i=0;i<inorder.length;i++)
-             inorder[i] = preorder[i];
-
-             Arrays.sort(inorder);
-
-             return bstFromPreorderAndInorder(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
+           return createBST(preorder,Integer.MAX_VALUE);
 
     }
 }
