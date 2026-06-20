72ef2d79d4751e146f1017d09c8bb04cfd3923a8
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-20 13:16:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (63.38%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,52 +13,84 @@
  *     }
  * }
  */
+
+ // Approach 1 : Morris Traversal for Inorder !!!
+ // TC : O(n) 
+ // SC : O(1) [no extra space]
+
+
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+         
+//             TreeNode curr = root;
+
+//            TreeNode lastElmOfIn = null;
+
+//             while(curr != null)
+//             {
+//                 if(curr.left == null)
+//                 {
+//                     if(lastElmOfIn != null && curr.val <= lastElmOfIn.val)
+//                     return false;
+                        
+//                     lastElmOfIn = curr;
+
+//                     curr = curr.right;
+//                 }
+//                 else
+//                 {
+                  
+//                   TreeNode pred = curr.left;
+
+//                   while(pred.right != null && pred.right != curr)
+//                   pred = pred.right;
+
+//                   if(pred.right == null)
+//                   {
+//                      pred.right = curr;
+//                      curr = curr.left;
+//                   }
+//                   else
+//                   {
+//                       pred.right = null;
+                       
+//                        if(curr.val <= lastElmOfIn.val)
+//                        return false;
+
+//                        lastElmOfIn = curr;
+
+//                       curr = curr.right;
+//                   }
+
+//                 }
+//             }
+
+//             return true;
+//     }
+// }
+
+
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+
+
 class Solution {
+
+    boolean isValidBST(TreeNode root, long leftBound , long rightBound)
+    {
+        if(root == null)
+        return true;
+
+        if(root.val <= leftBound || root.val >= rightBound)
+        return false;
+
+        return isValidBST(root.left,leftBound,root.val) && isValidBST(root.right,root.val,rightBound);
+    }
+
+
     public boolean isValidBST(TreeNode root) {
          
-            TreeNode curr = root;
-
-           TreeNode lastElmOfIn = null;
-
-            while(curr != null)
-            {
-                if(curr.left == null)
-                {
-                    if(lastElmOfIn != null && curr.val <= lastElmOfIn.val)
-                    return false;
-                        
-                    lastElmOfIn = curr;
-
-                    curr = curr.right;
-                }
-                else
-                {
-                  
-                  TreeNode pred = curr.left;
-
-                  while(pred.right != null && pred.right != curr)
-                  pred = pred.right;
-
-                  if(pred.right == null)
-                  {
-                     pred.right = curr;
-                     curr = curr.left;
-                  }
-                  else
-                  {
-                      pred.right = null;
-                       
-                       if(curr.val <= lastElmOfIn.val)
-                       return false;
-
-                       lastElmOfIn = curr;
-
-                      curr = curr.right;
-                  }
-
-                }
-            }
-
-            return true;
+          return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);  
     }
 }
