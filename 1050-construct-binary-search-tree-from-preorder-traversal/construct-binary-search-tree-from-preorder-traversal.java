60c7377660bc107126d425d7c04a9bc2c25a004f
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-21 13:17:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (94.31%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -15,7 +15,55 @@
 //  */
 
 
-// Approach 1 : Creating BST from Preorder and Inorder !!!
+class Solution {
+
+    void insertIntoBST(TreeNode root,int key)
+    {
+             TreeNode prev = null;
+             TreeNode curr = root;
+
+             while(curr != null)
+             {
+                if(key < curr.val)
+                {
+                  prev = curr;
+                  curr = curr.left;
+                } 
+                else
+                {
+                  prev = curr;
+                  curr = curr.right;
+                }
+             }
+
+             if(key > prev.val)
+             prev.right = new TreeNode(key);
+             else
+             prev.left = new TreeNode(key);
+
+    }
+   
+    public TreeNode bstFromPreorder(int[] preorder) {
+           
+            TreeNode root = new TreeNode(preorder[0]);
+
+            for(int i=1;i<preorder.length;i++)
+            insertIntoBST(root,preorder[i]);
+
+            return root;
+
+    }
+}
+
+
+
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+// Approach 2 : Creating BST from Preorder and Inorder !!!
 // Hint : To find the inorder --> just sort the preorder since we already have preorder of a BST
 // TC : O(nlogn) [sorting preorder to find inorder] + O(n) [recursive DFS to create BST]
 // SC : O(n) [recursive stack space i.e height of tree in case of degenerate BST] + O(n) [extra inorder[] array]
@@ -67,10 +115,53 @@
 
 
 
-/*-------------------------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------------------------*/
 
 
 
+
+// Approach 3 : Recursive DFS + Range Concept(Use lowerBound and upperBound) + Global idx pointer !!!
+// Watch this video : https://www.youtube.com/watch?v=UmJT3j26t1I&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=48
+// TC : O(n) [in case of degenerate BST]
+// SC : O(n) [ recursive stack space or height of BST in case of degenerate BST]
+
+// class Solution {
+   
+//     int idx = 0;
+
+//     TreeNode createBST(int[] preorder, int lowerBound, int upperBound)
+//     {
+//           if(idx == preorder.length || preorder[idx] < lowerBound || preorder[idx] > upperBound)
+//           return null;
+
+//           TreeNode root = new TreeNode(preorder[idx]);
+//           idx++;
+
+//           root.left = createBST(preorder,lowerBound,root.val);
+//           root.right = createBST(preorder,root.val,upperBound);
+
+//           return root;
+
+//     }
+//     public TreeNode bstFromPreorder(int[] preorder) {
+           
+//            return createBST(preorder,Integer.MIN_VALUE,Integer.MAX_VALUE);
+
+//     }
+// }
+
+
+
+
+/*---------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+// Approach 4 : Recursive DFS + Range Concept(use only upperBound) + Global idx pointer !!!
+// Watch this video : https://www.youtube.com/watch?v=UmJT3j26t1I&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=48
+// TC : O(n) [in case of degenerate BST]
+// SC : O(n) [ recursive stack space or height of BST in case of degenerate BST]
 
 // class Solution {
    
@@ -94,33 +185,3 @@
 
 //     }
 // }
-
-
-
-/*-------------------------------------------------------------------------------------------------------------------*/
-
-
-class Solution {
-   
-    int idx = 0;
-
-    TreeNode createBST(int[] preorder, int lowerBound, int upperBound)
-    {
-          if(idx == preorder.length || preorder[idx] < lowerBound || preorder[idx] > upperBound)
-          return null;
-
-          TreeNode root = new TreeNode(preorder[idx]);
-          idx++;
-
-          root.left = createBST(preorder,lowerBound,root.val);
-          root.right = createBST(preorder,root.val,upperBound);
-
-          return root;
-
-    }
-    public TreeNode bstFromPreorder(int[] preorder) {
-           
-           return createBST(preorder,Integer.MIN_VALUE,Integer.MAX_VALUE);
-
-    }
-}
