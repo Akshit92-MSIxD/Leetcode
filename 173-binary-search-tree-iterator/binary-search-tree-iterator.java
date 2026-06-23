d45f439c5d2fad1694c323e9f8982d5932d52434
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-23 18:23:10 +0530

    Time: 17 ms (95.89%) | Memory: 50.2 MB (59.97%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -13,129 +13,145 @@
  *     }
  * }
  */
-// class BSTIterator {
-    
-//     TreeNode curr = null;
-//     TreeNode head = null;
-//     TreeNode tail = null;
 
-//     public BSTIterator(TreeNode root) {
+// Note : I have written two approaches for this problem below . Approach 2 is optimal one !!!
+
+
+
+// Approach 1 : Transform given BST to Sorted Inorder Linked List !!! (My Brute force method !!! :)
+// TC for Constructor(BSTIterator) = O(n)
+// TC for next() = O(1)
+// TC for hasNext() = O(1)
+
+class BSTIterator {
+    
+    TreeNode curr = null;
+    TreeNode head = null;
+    TreeNode tail = null;
+
+    public BSTIterator(TreeNode root) {
            
-//            TreeNode[] endPoints = convertBSTintoInorderList(root);
+           TreeNode[] endPoints = convertBSTintoInorderList(root);
 
-//            head = endPoints[0];
-//            tail = endPoints[1];
+           head = endPoints[0];
+           tail = endPoints[1];
 
-//     }
+    }
 
-//     TreeNode[] convertBSTintoInorderList(TreeNode root)
-//     {
-//          if(root == null)
-//             return new TreeNode[]{null,null};
+    TreeNode[] convertBSTintoInorderList(TreeNode root)
+    {
+         if(root == null)
+            return new TreeNode[]{null,null};
 
 
-//          TreeNode[] leftSubtreeEndPoints = convertBSTintoInorderList(root.left);
-//          TreeNode[] rightSubtreeEndPoints = convertBSTintoInorderList(root.right);
+         TreeNode[] leftSubtreeEndPoints = convertBSTintoInorderList(root.left);
+         TreeNode[] rightSubtreeEndPoints = convertBSTintoInorderList(root.right);
 
-//          TreeNode leftHead = leftSubtreeEndPoints[0];
-//          TreeNode leftTail = leftSubtreeEndPoints[1];
+         TreeNode leftHead = leftSubtreeEndPoints[0];
+         TreeNode leftTail = leftSubtreeEndPoints[1];
 
-//          TreeNode rightHead = rightSubtreeEndPoints[0];
-//          TreeNode rightTail = rightSubtreeEndPoints[1];
+         TreeNode rightHead = rightSubtreeEndPoints[0];
+         TreeNode rightTail = rightSubtreeEndPoints[1];
 
-//          root.left = null;
-//          root.right = null;
+         root.left = null;
+         root.right = null;
 
-//          if(leftTail != null)
-//          {
-//             leftTail.right = root;
-//             root.right = rightHead;
+         if(leftTail != null)
+         {
+            leftTail.right = root;
+            root.right = rightHead;
             
-//             if(rightHead != null)
-//             return new TreeNode[]{leftHead,rightTail};
-//             else
-//             return new TreeNode[]{leftHead,root};
-//          }
+            if(rightHead != null)
+            return new TreeNode[]{leftHead,rightTail};
+            else
+            return new TreeNode[]{leftHead,root};
+         }
 
-//          root.right = rightHead;
+         root.right = rightHead;
          
-//          if(rightHead != null)
-//          return new TreeNode[]{root,rightTail};
+         if(rightHead != null)
+         return new TreeNode[]{root,rightTail};
 
-//          return new TreeNode[]{root,root};
-//     }
+         return new TreeNode[]{root,root};
+    }
     
-//     public int next() {
+    public int next() {
        
-//        if(curr == null)
-//        curr = head;
-//        else
-//        curr = curr.right;
+       if(curr == null)
+       curr = head;
+       else
+       curr = curr.right;
 
-//        return curr.val;
+       return curr.val;
      
-//     }
+    }
     
-//     public boolean hasNext() {
+    public boolean hasNext() {
         
-//         if(curr == null)
-//         return true;
+        if(curr == null)
+        return true;
 
-//         if(curr.right == null)
-//         return false;
+        if(curr.right == null)
+        return false;
 
-//         return true;
-//     }
-// }
+        return true;
+    }
+}
 
 
 
 /*----------------------------------------------------------------------------------------------------------*/
 
 
+// Approach 2 : Using Iterative Inorder DFS using stack !!! (By Striver !!!)
+// TC for next() ~ O(1) [amortized]
+// TC for hasNext() = O(1)
+// TC for Constructor (BSTIterator) = O(h) [left height of the BST]
+// SC : O(h) [height of the BST]
 
-class BSTIterator {
+
+// class BSTIterator {
     
-    Stack<TreeNode> st = new Stack<>();
+//     Stack<TreeNode> st = new Stack<>();
  
-    public BSTIterator(TreeNode root) {
+//     public BSTIterator(TreeNode root) {
            
-          while(root != null)
-          {
-            st.push(root);
-            root = root.left;
-          }
+//           while(root != null)
+//           {
+//             st.push(root);
+//             root = root.left;
+//           }
 
-    }
+//     }
 
     
-    public int next() {
+//     public int next() {
        
-       TreeNode top = st.peek();
-       st.pop();
+//        TreeNode top = st.peek();
+//        st.pop();
        
-       int nextElm = top.val;
+//        int nextElm = top.val;
 
-       TreeNode curr = top.right;
+//        TreeNode curr = top.right;
 
-       while(curr != null)
-       {
-           st.push(curr);
-           curr = curr.left;
-       }
+//        while(curr != null)
+//        {
+//            st.push(curr);
+//            curr = curr.left;
+//        }
 
-       return nextElm; 
-    }
+//        return nextElm; 
+//     }
     
-    public boolean hasNext() {
+//     public boolean hasNext() {
         
-        if(st.isEmpty())
-        return false;
+//         if(st.isEmpty())
+//         return false;
 
-        return true;
+//         return true;
           
-    }
-}
+//     }
+// }
 
 
 
