e79753455352374a92a740adbacf05b07ca50b18
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-23 18:11:59 +0530

    Time: 17 ms (95.89%) | Memory: 50.5 MB (8.03%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -13,82 +13,134 @@
  *     }
  * }
  */
+// class BSTIterator {
+    
+//     TreeNode curr = null;
+//     TreeNode head = null;
+//     TreeNode tail = null;
+
+//     public BSTIterator(TreeNode root) {
+           
+//            TreeNode[] endPoints = convertBSTintoInorderList(root);
+
+//            head = endPoints[0];
+//            tail = endPoints[1];
+
+//     }
+
+//     TreeNode[] convertBSTintoInorderList(TreeNode root)
+//     {
+//          if(root == null)
+//             return new TreeNode[]{null,null};
+
+
+//          TreeNode[] leftSubtreeEndPoints = convertBSTintoInorderList(root.left);
+//          TreeNode[] rightSubtreeEndPoints = convertBSTintoInorderList(root.right);
+
+//          TreeNode leftHead = leftSubtreeEndPoints[0];
+//          TreeNode leftTail = leftSubtreeEndPoints[1];
+
+//          TreeNode rightHead = rightSubtreeEndPoints[0];
+//          TreeNode rightTail = rightSubtreeEndPoints[1];
+
+//          root.left = null;
+//          root.right = null;
+
+//          if(leftTail != null)
+//          {
+//             leftTail.right = root;
+//             root.right = rightHead;
+            
+//             if(rightHead != null)
+//             return new TreeNode[]{leftHead,rightTail};
+//             else
+//             return new TreeNode[]{leftHead,root};
+//          }
+
+//          root.right = rightHead;
+         
+//          if(rightHead != null)
+//          return new TreeNode[]{root,rightTail};
+
+//          return new TreeNode[]{root,root};
+//     }
+    
+//     public int next() {
+       
+//        if(curr == null)
+//        curr = head;
+//        else
+//        curr = curr.right;
+
+//        return curr.val;
+     
+//     }
+    
+//     public boolean hasNext() {
+        
+//         if(curr == null)
+//         return true;
+
+//         if(curr.right == null)
+//         return false;
+
+//         return true;
+//     }
+// }
+
+
+
+/*----------------------------------------------------------------------------------------------------------*/
+
+
+
 class BSTIterator {
     
-    TreeNode curr = null;
-    TreeNode head = null;
-    TreeNode tail = null;
-
+    Stack<TreeNode> st = new Stack<>();
+ 
     public BSTIterator(TreeNode root) {
            
-           TreeNode[] endPoints = convertBSTintoInorderList(root);
-
-           head = endPoints[0];
-           tail = endPoints[1];
+          while(root != null)
+          {
+            st.push(root);
+            root = root.left;
+          }
 
     }
 
-    TreeNode[] convertBSTintoInorderList(TreeNode root)
-    {
-         if(root == null)
-         {
-            return new TreeNode[]{null,null};
-         }
-
-
-         TreeNode[] leftSubtreeEndPoints = convertBSTintoInorderList(root.left);
-         TreeNode[] rightSubtreeEndPoints = convertBSTintoInorderList(root.right);
-
-         TreeNode leftHead = leftSubtreeEndPoints[0];
-         TreeNode leftTail = leftSubtreeEndPoints[1];
-
-         TreeNode rightHead = rightSubtreeEndPoints[0];
-         TreeNode rightTail = rightSubtreeEndPoints[1];
-
-         root.left = null;
-         root.right = null;
-
-         if(leftTail != null)
-         {
-            leftTail.right = root;
-            root.right = rightHead;
-            
-            if(rightHead != null)
-            return new TreeNode[]{leftHead,rightTail};
-            else
-            return new TreeNode[]{leftHead,root};
-         }
-
-         root.right = rightHead;
-         
-         if(rightHead != null)
-         return new TreeNode[]{root,rightTail};
-
-         return new TreeNode[]{root,root};
-    }
     
     public int next() {
        
-       if(curr == null)
-       curr = head;
-       else
-       curr = curr.right;
+       TreeNode top = st.peek();
+       st.pop();
+       
+       int nextElm = top.val;
 
-       return curr.val;
-     
+       TreeNode curr = top.right;
+
+       while(curr != null)
+       {
+           st.push(curr);
+           curr = curr.left;
+       }
+
+       return nextElm; 
     }
     
     public boolean hasNext() {
         
-        if(curr == null)
-        return true;
-
-        if(curr.right == null)
+        if(st.isEmpty())
         return false;
 
         return true;
+          
     }
 }
+
+
+
+
+
 
 /**
  * Your BSTIterator object will be instantiated and called as such:
