ee32516cd95826cd1d13e55d1a83bb840dd10283
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-24 18:21:59 +0530

    Time: 2 ms (97.79%) | Memory: 47.3 MB (54.96%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -15,43 +15,52 @@
  */
 
 
-// class Solution {
+ // Note : I have written two approaches for this problem below . Approach 2 is most optimal and imp !!!
 
-//     void dfs(TreeNode root,List<Integer> inorder)
-//     {
-//         if(root == null)
-//         return;
 
-//         dfs(root.left,inorder);
-//         inorder.add(root.val);
-//         dfs(root.right,inorder);
-//     }
 
-//     public boolean findTarget(TreeNode root, int k) {
+
+// Approach 1 : Brute Force (Find Sorted Inorder Array + 2 pointers)
+// TC : O(n) [dfs] + O(n) [two pointer traversal]
+// SC : + O(n) [extra Inorder array]  + O(h) [recursive stack space i.e height of the BST] 
+
+class Solution {
+
+    void dfs(TreeNode root,List<Integer> inorder)
+    {
+        if(root == null)
+        return;
+
+        dfs(root.left,inorder);
+        inorder.add(root.val);
+        dfs(root.right,inorder);
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
         
-//        List<Integer> inorder = new ArrayList<>();
+       List<Integer> inorder = new ArrayList<>();
         
-//           dfs(root,inorder); 
+          dfs(root,inorder); 
 
-//         int left = 0;
-//         int right = inorder.size() - 1;
+        int left = 0;
+        int right = inorder.size() - 1;
 
-//         while(left < right)
-//         {
-//             int pairSum = inorder.get(left) + inorder.get(right);
+        while(left < right)
+        {
+            int pairSum = inorder.get(left) + inorder.get(right);
 
-//             if(pairSum == k)
-//                 return true;
-//             else if(pairSum > k)
-//                 right--;
-//             else
-//                 left++;
-//         }
+            if(pairSum == k)
+                return true;
+            else if(pairSum > k)
+                right--;
+            else
+                left++;
+        }
 
-//         return false;
+        return false;
 
-//     }
-// }
+    }
+}
 
 
 
@@ -61,86 +70,99 @@
 
 
 
+// Approach 2 : Using One stack to store ascending inorder (from left to Right) + another stack to store descending inorder(from Right to Left) + use of next() and before() function !!!
 
-class Solution {
+/* Prerequisite :  https://leetcode.com/problems/binary-search-tree-iterator/description/
+         You must know how to generate Inorder of a Tree using Stack (Iterative DFS Approach !!!) */
+
+// TC of initializeBothStacks : O(h)
+// TC of next() ~ O(1) [amortized]
+// TC of before() ~ O(1) [amortized]
+// TC of 2 pointer traversal : O(n)
+
+// SC : O(h*2) [for both stacks] ~ O(h)
+
+
+
+// class Solution {
     
-    Stack<TreeNode> ltrStack = new Stack<>();   // gives inorder in this fashion (Left To Right) in ascending order
-    Stack<TreeNode> rtlStack = new Stack<>();  // gives inorder in this fashion ( Right to Left) in descending order
+//     Stack<TreeNode> ltrStack = new Stack<>();   // gives inorder in this fashion (Left To Right) in ascending order
+//     Stack<TreeNode> rtlStack = new Stack<>();  // gives inorder in this fashion ( Right to Left) in descending order
     
 
-    void initializeBothStacks(TreeNode root)
-    {
-           TreeNode curr1 = root;
-           TreeNode curr2 = root;
+//     void initializeBothStacks(TreeNode root)
+//     {
+//            TreeNode curr1 = root;
+//            TreeNode curr2 = root;
 
-           while(curr1 != null)
-           {
-             ltrStack.add(curr1);
-             curr1 = curr1.left;
-           }
+//            while(curr1 != null)   // push all the left nodes from root
+//            {
+//              ltrStack.add(curr1);
+//              curr1 = curr1.left;
+//            }
 
-           while(curr2 != null)
-           {
-             rtlStack.add(curr2);
-             curr2 = curr2.right;
-           }
-    }
+//            while(curr2 != null) // push all the right nodes from root
+//            {
+//              rtlStack.add(curr2);
+//              curr2 = curr2.right;
+//            }
+//     }
 
-    int next()
-    {
-        TreeNode top = ltrStack.pop();
+//     int next()
+//     {
+//         TreeNode top = ltrStack.pop();
 
-        int nextElm = top.val;
+//         int nextElm = top.val;
 
-        TreeNode curr = top.right;
+//         TreeNode curr = top.right;
 
-        while(curr != null)
-        {
-            ltrStack.push(curr);
-            curr = curr.left;
-        }
+//         while(curr != null)
+//         {
+//             ltrStack.push(curr);
+//             curr = curr.left;
+//         }
 
-        return nextElm;
-    }
+//         return nextElm;
+//     }
 
-    int before()
-    {
-        TreeNode top = rtlStack.pop();
+//     int before()
+//     {
+//         TreeNode top = rtlStack.pop();
 
-        int beforeElm = top.val;
+//         int beforeElm = top.val;
 
-        TreeNode curr = top.left;
+//         TreeNode curr = top.left;
 
-        while(curr != null)
-        {
-            rtlStack.push(curr);
-            curr = curr.right;
-        }
+//         while(curr != null)
+//         {
+//             rtlStack.push(curr);
+//             curr = curr.right;
+//         }
 
-        return beforeElm;
-    }
+//         return beforeElm;
+//     }
 
 
-    public boolean findTarget(TreeNode root, int k) {
+//     public boolean findTarget(TreeNode root, int k) {
          
-        initializeBothStacks(root);
+//         initializeBothStacks(root);
          
-         int left = next();
-         int right = before();
+//          int left = next();
+//          int right = before();
 
-         while(left < right)
-         {
-             int pairSum = left + right;
+//          while(left < right)
+//          {
+//              int pairSum = left + right;
 
-             if(pairSum == k)
-               return true;
-             else if(pairSum > k)
-             right = before();
-             else
-             left = next();
-         }
+//              if(pairSum == k)
+//                return true;
+//              else if(pairSum > k)
+//              right = before();
+//              else
+//              left = next();
+//          }
 
-         return false;
+//          return false;
 
-    }
-}
+//     }
+// }
