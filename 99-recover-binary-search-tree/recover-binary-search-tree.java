c95fbd7ff416bc1fa8ed247f10bbe17cad8d89bb
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-26 11:46:56 +0530

    Time: 2 ms (17.13%) | Memory: 46.4 MB (68.66%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -13,39 +13,97 @@
  *     }
  * }
  */
+// class Solution {
+
+//     void dfs1(TreeNode root,List<Integer> inorder)
+//     {
+//             if(root == null)
+//             return;
+
+//             dfs1(root.left,inorder);
+//             inorder.add(root.val);
+//             dfs1(root.right,inorder);
+//     }
+    
+//     int idx = 0;
+
+//     void dfs2(TreeNode root,List<Integer> inorder)
+//     {
+//            if(root == null)
+//            return;
+
+//            dfs2(root.left,inorder);
+//            root.val = inorder.get(idx++);
+//            dfs2(root.right,inorder);
+//     }
+
+//     public void recoverTree(TreeNode root) {
+
+//         List<Integer> inorder = new ArrayList<>();
+
+//         dfs1(root,inorder);
+
+//         Collections.sort(inorder);
+
+//         dfs2(root,inorder);
+        
+//     }
+// }
+
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+
 class Solution {
 
-    void dfs1(TreeNode root,List<Integer> inorder)
+    void dfs(TreeNode root,List<TreeNode> inorder)
     {
-            if(root == null)
-            return;
+        if(root == null)
+        return;
 
-            dfs1(root.left,inorder);
-            inorder.add(root.val);
-            dfs1(root.right,inorder);
+        dfs(root.left,inorder);
+        inorder.add(root);
+        dfs(root.right,inorder);
+        
     }
-    
-    int idx = 0;
 
-    void dfs2(TreeNode root,List<Integer> inorder)
-    {
-           if(root == null)
-           return;
-
-           dfs2(root.left,inorder);
-           root.val = inorder.get(idx++);
-           dfs2(root.right,inorder);
-    }
 
     public void recoverTree(TreeNode root) {
-
-        List<Integer> inorder = new ArrayList<>();
-
-        dfs1(root,inorder);
-
-        Collections.sort(inorder);
-
-        dfs2(root,inorder);
         
+         List<TreeNode> inorder = new ArrayList<>();
+
+         dfs(root,inorder);
+
+         TreeNode n1 = null;
+
+          int i = 0;
+
+         while(i<inorder.size()-1)
+         {
+                if(inorder.get(i).val > inorder.get(i+1).val)
+                {
+                    n1 = inorder.get(i);
+                    break;
+                }
+                i++;
+         }
+
+         i++;
+
+         TreeNode n2 = null;
+
+         while(i<inorder.size())
+         {
+             if(inorder.get(i).val < n1.val)
+                n2 = inorder.get(i++);
+             else
+                break;
+         }
+
+
+         n1.val = n1.val + n2.val;
+         n2.val = n1.val - n2.val;
+         n1.val = n1.val - n2.val;
+    
     }
 }
