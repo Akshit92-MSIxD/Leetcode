c76623671f1df4a5c1d876eee1989a7357c46cf9
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-26 17:39:17 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (12.92%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -54,56 +54,127 @@
 /*-------------------------------------------------------------------------------------------------------*/
 
 
+// class Solution {
+
+//     void dfs(TreeNode root,List<TreeNode> inorder)
+//     {
+//         if(root == null)
+//         return;
+
+//         dfs(root.left,inorder);
+//         inorder.add(root);
+//         dfs(root.right,inorder);
+        
+//     }
+
+
+//     public void recoverTree(TreeNode root) {
+        
+//          List<TreeNode> inorder = new ArrayList<>();
+
+//          dfs(root,inorder);
+
+//          TreeNode n1 = null;
+
+//           int i = 0;
+
+//          while(i<inorder.size()-1)
+//          {
+//                 if(inorder.get(i).val > inorder.get(i+1).val)
+//                 {
+//                     n1 = inorder.get(i);
+//                     break;
+//                 }
+//                 i++;
+//          }
+
+//          i++;
+
+//          TreeNode n2 = null;
+
+//          while(i<inorder.size())
+//          {
+//              if(inorder.get(i).val < n1.val)
+//                 n2 = inorder.get(i++);
+//              else
+//                 break;
+//          }
+
+
+//          n1.val = n1.val + n2.val;
+//          n2.val = n1.val - n2.val;
+//          n1.val = n1.val - n2.val;
+    
+//     }
+// }
+
+
+
+
+/*----------------------------------------------------------------------------------------------------*/
+
+
+
+
 class Solution {
 
-    void dfs(TreeNode root,List<TreeNode> inorder)
-    {
-        if(root == null)
-        return;
-
-        dfs(root.left,inorder);
-        inorder.add(root);
-        dfs(root.right,inorder);
-        
-    }
-
-
     public void recoverTree(TreeNode root) {
-        
-         List<TreeNode> inorder = new ArrayList<>();
+         
+           TreeNode curr = root;
 
-         dfs(root,inorder);
+           TreeNode first = null;
+           TreeNode second = null;
 
-         TreeNode n1 = null;
+           TreeNode inorderPred = null;
 
-          int i = 0;
+           while(curr != null)
+           {
+              if(curr.left == null)
+              {
+                 if(first == null && inorderPred != null && curr.val < inorderPred.val)
+                     first = inorderPred;
 
-         while(i<inorder.size()-1)
-         {
-                if(inorder.get(i).val > inorder.get(i+1).val)
-                {
-                    n1 = inorder.get(i);
-                    break;
-                }
-                i++;
-         }
-
-         i++;
-
-         TreeNode n2 = null;
-
-         while(i<inorder.size())
-         {
-             if(inorder.get(i).val < n1.val)
-                n2 = inorder.get(i++);
-             else
-                break;
-         }
+                 if(first != null && curr.val < first.val)
+                     second = curr;
 
 
-         n1.val = n1.val + n2.val;
-         n2.val = n1.val - n2.val;
-         n1.val = n1.val - n2.val;
-    
+                 inorderPred = curr;
+                 curr = curr.right;
+              }
+              else
+              {
+                 TreeNode pred = curr.left;
+
+                 while(pred.right != null && pred.right != curr)
+                 pred = pred.right;
+
+                 if(pred.right == null)
+                 {
+                    pred.right = curr;
+                    curr = curr.left;
+                 }
+                 else
+                 {
+                     pred.right = null;
+
+                     if(first == null && curr.val < inorderPred.val)
+                     first = inorderPred;
+
+                     if(first != null && curr.val < first.val)
+                     second = curr;
+                     
+                     inorderPred = curr;
+                     curr = curr.right;
+                 }
+              }
+           }
+
+         
+         first.val = first.val ^ second.val;
+         second.val = first.val ^ second.val;
+         first.val = first.val ^ second.val;
+
+
+          
     }
 }
