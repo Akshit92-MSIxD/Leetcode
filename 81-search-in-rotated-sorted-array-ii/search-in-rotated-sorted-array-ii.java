482a004a27d789dbf1cd19e369ae8ab7deace76f
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-30 12:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (45.38%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,3 +1,9 @@
+
+// Approach 1 : Using Binary Search + Trim Search Space Concept
+// TC : O(logn) (average case)
+// TC : O(n) [ in worst case] [Ex : {3,3,3,3,3,3,3,3,3,3,3,3,3,3} , target = 0]
+// SC : O(1)
+
 class Solution {
     public boolean search(int[] nums, int target) {
           
@@ -12,7 +18,10 @@ class Solution {
                {
                  return true;
                }
-            
+               
+            // For this type of condition (bc konsa wala region sorted hai teeno to equal aarahe)
+            // Solution : trim down the left and right region continuously until this problem disappeared
+
                if(low <= high && nums[low] == nums[mid] && nums[mid] == nums[high])
                {
                    low++;
@@ -20,25 +29,27 @@ class Solution {
                    continue;
                }
 
+                  
+                // Identify the sorted region first
 
-                  if(nums[high] >= nums[mid])
+                  if(nums[high] >= nums[mid]) // right region is sorted region if this case is true   
                   {
-                      if(target > nums[mid] && target <= nums[high])
+                      if(target > nums[mid] && target <= nums[high])  // check if target lies in right sorted region
                       {
                          low = mid + 1;
                       }
-                      else
+                      else   // target lies in left non-sorted region
                       {
                         high = mid - 1;
                       }
                   }
-                  else                                                        
+                  else      // left region is sorted region if this case is true                                             
                   {
-                      if(target >= nums[low] && target < nums[mid])
+                      if(target >= nums[low] && target < nums[mid]) // check if target lies in left sorted region
                       {
                          high = mid - 1;
                       }
-                      else
+                      else     // target lies in right non-sorted region
                       {
                         low = mid + 1;
                       }
