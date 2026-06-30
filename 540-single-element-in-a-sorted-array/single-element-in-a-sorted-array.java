7d396037cb2133e7bdb481c1a8329579fd270392
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-30 16:44:27 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (20.85%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,15 +1,62 @@
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+
+//           int i=0;
+
+//           while(i<nums.length-1)
+//           {
+//              if(nums[i] != nums[i+1])
+//              return nums[i];
+//              i+=2;
+//           }
+          
+//           return nums[nums.length-1];
+//     }
+// }
+
+
 class Solution {
     public int singleNonDuplicate(int[] nums) {
 
-          int i=0;
+             int low = 0;
+             int high = nums.length - 1;
 
-          while(i<nums.length-1)
-          {
-             if(nums[i] != nums[i+1])
-             return nums[i];
-             i+=2;
-          }
-          
-          return nums[nums.length-1];
+             int singleElm = -1;
+
+             while(low<=high)
+             {
+                int mid = low + (high - low)/2;
+
+                if((mid-1 < 0 || nums[mid-1] != nums[mid]) && (mid+1>high || nums[mid] != nums[mid+1]))
+                {
+                    singleElm = nums[mid];
+                    break;
+                }
+
+                if(mid%2==0)
+                {
+                   if(nums[mid-1] == nums[mid])
+                   {
+                       high = mid - 1;
+                   }
+                   else
+                   {
+                       low = mid + 2;
+                   }
+                }
+                else
+                {
+                    if(nums[mid-1] == nums[mid])
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid - 1;
+                    }
+                }
+             }
+
+             return singleElm;
     }
 }
