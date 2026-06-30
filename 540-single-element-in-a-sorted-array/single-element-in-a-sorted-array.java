7c49c07121fe981a27dcfacc65dd5d944ad24535
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-06-30 17:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 52.6 MB (94.92%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,18 +1,18 @@
-class Solution {
-    public int singleNonDuplicate(int[] nums) {
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
 
-          int i=0;
+//           int i=0;
 
-          while(i<nums.length-1)
-          {
-             if(nums[i] != nums[i+1])
-             return nums[i];
-             i+=2;
-          }
+//           while(i<nums.length-1)
+//           {
+//              if(nums[i] != nums[i+1])
+//              return nums[i];
+//              i+=2;
+//           }
           
-          return nums[i];
-    }
-}
+//           return nums[i];
+//     }
+// }
 
 
 // class Solution {
@@ -60,3 +60,44 @@ class Solution {
 //              return singleElm;
 //     }
 // }
+
+
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+             
+             int n = nums.length;
+
+             if(n == 1)
+             return nums[0];
+
+             if(nums[0] != nums[1])
+             return nums[0];
+
+             if(nums[n-1] != nums[n-2])
+             return nums[n-1];
+
+             int low = 1;
+             int high = n-2;
+
+             int singleElm = -1;
+
+             while(low<=high)
+             {
+                int mid = low + (high - low)/2;
+
+                if(nums[mid-1] != nums[mid]  && nums[mid] != nums[mid+1])
+                {
+                    singleElm = nums[mid];
+                    break;
+                }
+
+                if(mid%2==1 && nums[mid] == nums[mid-1] || mid%2==0 && nums[mid] == nums[mid+1])
+                   low = mid + 1;
+                else
+                   high = mid - 1;
+             }
+
+             return singleElm;
+    }
+}
