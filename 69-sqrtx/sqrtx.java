b5e04cbfd31d66f25a73b75810ec70883f35a471
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-07-01 19:27:18 +0530

    Time: 1 ms (97.72%) | Memory: 42.4 MB (78.07%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,3 +1,8 @@
+
+// Approach : Using Binary Search + Consider low and high as numbers and not the indices !!!
+// TC : O(logn)
+// SC : O(1)
+
 class Solution {
     public int mySqrt(int x) {
          
@@ -12,16 +17,16 @@ class Solution {
           {
              int mid = low + (high - low)/2;
 
-             if(mid == x/mid)
+             if(mid == x/mid)    // means mid*mid == x (if this is true then mid is the square root of x)
              return mid;
 
-             if(mid > x/mid)
+             if(mid > x/mid)    // means mid*mid > x (if this is true that means mid is not the square root of x since its product with itself is greater than x so go left to find its valid square root)
              {
                 high = mid - 1;
              }
-             else
+             else     // means mid*mid < x (if this is true that means mid may or may not be the square root of x so include it in your answer and move right to find if there exists a valid square root in the right)
              {
-                sqrt = mid;
+                sqrt = mid;   // include the mid in your answer since it may or may not be the answer !!!
                 low = mid + 1;
              }
           }
