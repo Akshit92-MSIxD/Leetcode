6d3fe6a4db5463bfa853c7856a565e571a27f64a
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-07-01 19:43:16 +0530

    Time: 1 ms (97.72%) | Memory: 42.5 MB (61.19%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,6 @@
 
 // Approach : Using Binary Search + Consider low and high as numbers and not the indices !!!
+// Concept : Indirectly we are finding the "floor of x" or the "maximum number" m such that m*m <= n !!!
 // TC : O(logn)
 // SC : O(1)
 
@@ -16,18 +17,16 @@ class Solution {
           while(low <= high)
           {
              int mid = low + (high - low)/2;
-
-             if(mid == x/mid)    // means mid*mid == x (if this is true then mid is the square root of x)
-             return mid;
-
-             if(mid > x/mid)    // means mid*mid > x (if this is true that means mid is not the square root of x since its product with itself is greater than x so go left to find its valid square root)
-             {
-                high = mid - 1;
-             }
-             else     // means mid*mid < x (if this is true that means mid may or may not be the square root of x so include it in your answer and move right to find if there exists a valid square root in the right)
+              
+              // means mid*mid <= x (if this is true then mid may or may not be the square root of x so better include it in your answer and move right to find if there exists a valid square root in the right)
+             if(mid <= x/mid)
              {
                 sqrt = mid;   // include the mid in your answer since it may or may not be the answer !!!
                 low = mid + 1;
+             }   
+             else   // means mid*mid > x (if this is true that means mid is not the square root of x since its product with itself is greater than x so go left to find its valid square root)
+             {
+                high = mid - 1;
              }
           }
 
