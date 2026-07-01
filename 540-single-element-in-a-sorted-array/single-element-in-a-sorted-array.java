c00111d6c32c0b80e626ce28b9a860521574f038
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-07-01 13:02:21 +0530

    Time: 1 ms (24.47%) | Memory: 52.6 MB (95.00%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,18 +1,57 @@
-// class Solution {
-//     public int singleNonDuplicate(int[] nums) {
+// Note : I have written three approaches for this problem below . Approach 2 and 3 are most optimal !!!
+// ***Note : Please read both Approach 2 and 3 !!!
 
-//           int i=0;
 
-//           while(i<nums.length-1)
-//           {
-//              if(nums[i] != nums[i+1])
-//              return nums[i];
-//              i+=2;
-//           }
+
+
+
+// Approach 1 : Using Optmized Linear Search
+// TC : O(n/2)
+// SC : O(1)
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+
+          int i=0;
+
+          while(i<nums.length-1)
+          {
+             if(nums[i] != nums[i+1])
+             return nums[i];
+             i+=2;
+          }
           
-//           return nums[i];
-//     }
-// }
+          return nums[i];
+    }
+}
+
+
+
+
+
+/*----------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+// Approach 2 : Using Binary Search + even-odd/odd-even index pair for couples Concept !!! (Written by me !!!)
+
+// Hint : A elm is a single elm if and only if neither of its adjacent left or right element is equal to it.
+
+// Watch this : https://www.youtube.com/watch?v=AZOmHuHadxQ&list=PLF6ChxadzFf8vjafLIxxbKUfarW4V4IOh&index=8
+
+/* Concept :
+
+   -> even-odd index pair for couples -> means we are standing in left region/side of single elm -> so go 
+   towards right direction to find single elm
+
+   -> odd-even index pair for couples -> means we are standing in right region/side of single elm -> so go towards left direction to find the single elm
+                                                            */
+
+// TC : O(logn)
+// SC : O(1)
+
 
 
 // class Solution {
@@ -63,36 +102,64 @@
 
 
 
-class Solution {
-    public int singleNonDuplicate(int[] nums) {
+
+
+
+/*-----------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+// Approach 3 : Using Binary Search + even-odd/odd-even index pair for couples Concept + Trimming first and last index and start binary search from  index 1 - index n-2 !!! -> to avoid multiple if/else conditions !!! (By Striver !!!)
+
+// Watch this : https://www.youtube.com/watch?v=AZOmHuHadxQ&list=PLF6ChxadzFf8vjafLIxxbKUfarW4V4IOh&index=8
+
+// Hint : A elm is a single elm if and only if neither of its adjacent left or right element is equal to it.
+
+/* Concept :
+
+   -> even-odd index pair for couples -> means we are standing in left region/side of single elm -> so go 
+   towards right direction to find single elm
+
+   -> odd-even index pair for couples -> means we are standing in right region/side of single elm -> so go towards left direction to find the single elm
+                                                            */
+
+// TC : O(logn)
+// SC : O(1)
+
+
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
              
-             int n = nums.length;
+//              int n = nums.length;
 
-             if(n == 1)
-             return nums[0];
+//              if(n == 1)
+//              return nums[0];
 
-             if(nums[0] != nums[1])
-             return nums[0];
+//              if(nums[0] != nums[1])
+//              return nums[0];
 
-             if(nums[n-1] != nums[n-2])
-             return nums[n-1];
+//              if(nums[n-1] != nums[n-2])
+//              return nums[n-1];
 
-             int low = 1;
-             int high = n-2;
+//              int low = 1;
+//              int high = n-2;
 
-             while(low<=high)
-             {
-                int mid = low + (high - low)/2;
+//              while(low<=high)
+//              {
+//                 int mid = low + (high - low)/2;
 
-                if(nums[mid-1] != nums[mid]  && nums[mid] != nums[mid+1])
-                return nums[mid];
+//                 if(nums[mid-1] != nums[mid]  && nums[mid] != nums[mid+1])
+//                 return nums[mid];
 
-                if(mid%2==1 && nums[mid] == nums[mid-1] || mid%2==0 && nums[mid] == nums[mid+1])
-                   low = mid + 1;
-                else
-                   high = mid - 1;
-             }
+//                 if(mid%2==1 && nums[mid] == nums[mid-1] || mid%2==0 && nums[mid] == nums[mid+1])
+//                    low = mid + 1;
+//                 else
+//                    high = mid - 1;
+//              }
 
-             return -1;   // dummy return statement
-    }
-}
+//              return -1;   // dummy return statement
+//     }
+// }
