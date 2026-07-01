2bf646c98e1945900faf5d6c3a34b6c4d5f3aad2
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-07-01 18:51:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (55.35%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,57 +1,42 @@
-// class Solution {
-//     public int findPeakElement(int[] nums) {
-        
-//         if(nums.length == 1)
-//         return 0;
-
-//         int n = nums.length;
-
-//         if(nums[0] > nums[1])
-//         return 0;
-
-//         if(nums[n-1] > nums[n-2])
-//         return n-1;
-
-//         int low = 1;
-//         int high = n-2;
-
-//         while(low<=high)
-//         {
-//             int mid = low + (high - low)/2;
-
-//             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
-//             return mid;
-
-//             if(nums[mid] > nums[mid+1])
-//             high = mid - 1;
-//             else
-//             low = mid + 1;
-//         }
-
-//         return -1;
-//     }
-// }
 
 
+// Approach : Using Binary Search + Graph Slope Peak Concept + Trimming Oth and n-1 th index !!!
+// Hint : Please draw a 2D graph on paper to visualize this concept in a better way !!!
+// TC : O(logn)
+// SC : O(1)
 
 class Solution {
     public int findPeakElement(int[] nums) {
         
+
         int n = nums.length;
 
-        int low = 0;
-        int high = n-1;
+        if(nums.length == 1)
+        return 0;
 
-        while(low<high)
+        if(nums[0] > nums[1])
+        return 0;
+
+        if(nums[n-1] > nums[n-2])
+        return n-1;
+
+        int low = 1;
+        int high = n-2;
+
+
+        while(low<=high)
         {
             int mid = low + (high - low)/2;
 
-            if(nums[mid] > nums[mid+1])
-            high = mid;
-            else
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) // check if mid is peak or not
+            return mid;
+
+            if(nums[mid] > nums[mid+1]) // if this is true thats means mid is on downward slope so peak is on the left
+            high = mid - 1;
+            else                // else mid is on the upward slope  so peek is on the right
             low = mid + 1;
         }
 
-        return low;
+        return -1;
     }
 }
