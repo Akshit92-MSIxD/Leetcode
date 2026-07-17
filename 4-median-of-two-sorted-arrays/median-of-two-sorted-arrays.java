b80e46e6cfc85e011b95e4bc3606b416ff421221
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-07-17 18:32:35 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (87.53%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -31,51 +31,135 @@
 // }
 
 
+
+
+/*------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+// class Solution {
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+           
+//             int m  = nums1.length;
+//             int n = nums2.length;
+
+//             int i=0,j=0,k=0;
+
+//             int curr = Integer.MIN_VALUE;
+//             int prev = curr;
+
+//             int end = (m+n)/2;
+
+//             while(i<m && j<n && k<=end)
+//             {
+//                 if(nums1[i]<=nums2[j])
+//                 {
+//                     prev = curr;
+//                     curr = nums1[i++];      
+//                 }
+//                 else
+//                 {
+//                    prev = curr;
+//                    curr = nums2[j++];
+//                 }
+//                 k++;
+//             }
+
+//             while(i<m && k<=end)
+//             {
+//                 prev = curr;
+//                 curr = nums1[i++];
+//                 k++;
+//             }
+
+//             while(j<n && k<=end)
+//             {
+//                 prev = curr;
+//                 curr = nums2[j++];
+//                 k++;
+//             }
+
+//             if((m+n)%2==0)
+//             return prev + (curr-prev)/2.0;
+
+//             return curr + 0.00;
+//     }
+// }
+
+
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
            
             int m  = nums1.length;
             int n = nums2.length;
 
-            int i=0,j=0,k=0;
+           int[] smallArray = null;
+           int[] largeArray = null;
 
-            int curr = Integer.MIN_VALUE;
-            int prev = curr;
+           if(m <= n)
+           {
+             smallArray = nums1;
+             largeArray = nums2;
+           }
+           else
+           {
+             smallArray = nums2;
+             largeArray = nums1;
+           }
 
-            int end = (m+n)/2;
 
-            while(i<m && j<n && k<=end)
-            {
-                if(nums1[i]<=nums2[j])
-                {
-                    prev = curr;
-                    curr = nums1[i++];      
-                }
-                else
-                {
-                   prev = curr;
-                   curr = nums2[j++];
-                }
-                k++;
-            }
+           int low = 0;
+           int high = smallArray.length;
 
-            while(i<m && k<=end)
-            {
-                prev = curr;
-                curr = nums1[i++];
-                k++;
-            }
+           int leftHalf = (m+n+1)/2;
 
-            while(j<n && k<=end)
-            {
-                prev = curr;
-                curr = nums2[j++];
-                k++;
-            }
 
-            if((m+n)%2==0)
-            return prev + (curr-prev)/2.0;
+           while(low <= high)
+           {
+               int mid1 = low + (high - low)/2;
+               int mid2 = leftHalf - mid1;
 
-            return curr + 0.00;
+               int l1 = Integer.MIN_VALUE;
+               int l2 = Integer.MIN_VALUE;
+
+               int r1 = Integer.MAX_VALUE;
+               int r2 = Integer.MAX_VALUE;
+
+               if(mid1 > 0)
+               l1 = smallArray[mid1-1];
+
+               if(mid2 > 0)
+               l2 = largeArray[mid2-1];
+
+               if(mid1 < smallArray.length)
+               r1 = smallArray[mid1];
+
+               if(mid2 < largeArray.length)
+               r2 = largeArray[mid2];
+
+
+               if(l1 <= r2 && l2 <= r1)
+               {
+
+                   if((m+n)%2 == 0)
+                   {
+                    return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
+                   }
+
+                   return Math.max(l1,l2);
+               }
+               else if(l1 > r2)
+               {
+                  high = mid1 - 1;
+               }
+               else
+               {
+                 low = mid1 + 1;
+               }
+           }
+
+           return -1; // dummy return value;
     }
 }
