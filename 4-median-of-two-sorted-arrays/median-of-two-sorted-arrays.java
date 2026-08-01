2e8323ef99351b305f68026f87f8ced6846978fa
Akshit Rawat <77582965+Akshit92-MSIxD@users.noreply.github.com>
2026-08-01 17:30:22 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (72.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -89,77 +89,126 @@
 
 
 
+/*-----------------------------------------------------------------------------------------------------*/
+
+
+
+// class Solution {
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+           
+//             int m  = nums1.length;
+//             int n = nums2.length;
+
+//            int[] smallArray = null;
+//            int[] largeArray = null;
+
+//            if(m <= n)
+//            {
+//              smallArray = nums1;
+//              largeArray = nums2;
+//            }
+//            else
+//            {
+//              smallArray = nums2;
+//              largeArray = nums1;
+//            }
+
+
+//            int low = 0;
+//            int high = smallArray.length;
+
+//            int leftHalf = (m+n+1)/2;
+
+
+//            while(low <= high)
+//            {
+//                int mid1 = low + (high - low)/2;
+//                int mid2 = leftHalf - mid1;
+
+//                int l1 = Integer.MIN_VALUE;
+//                int l2 = Integer.MIN_VALUE;
+
+//                int r1 = Integer.MAX_VALUE;
+//                int r2 = Integer.MAX_VALUE;
+
+//                if(mid1 > 0)
+//                l1 = smallArray[mid1-1];
+
+//                if(mid2 > 0)
+//                l2 = largeArray[mid2-1];
+
+//                if(mid1 < smallArray.length)
+//                r1 = smallArray[mid1];
+
+//                if(mid2 < largeArray.length)
+//                r2 = largeArray[mid2];
+
+
+//                if(l1 <= r2 && l2 <= r1)
+//                {
+
+//                    if((m+n)%2 == 0)
+//                    {
+//                     return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
+//                    }
+
+//                    return Math.max(l1,l2);
+//                }
+//                else if(l1 > r2)
+//                {
+//                   high = mid1 - 1;
+//                }
+//                else
+//                {
+//                  low = mid1 + 1;
+//                }
+//            }
+
+//            return -1; // dummy return value;
+//     }
+// }
+
+
+
+/*----------------------------------------------------------------------------------------------------*/
+
+
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
            
-            int m  = nums1.length;
-            int n = nums2.length;
+            
+             if(nums1.length > nums2.length)
+             return findMedianSortedArrays(nums2,nums1);
 
-           int[] smallArray = null;
-           int[] largeArray = null;
+             int n1 = nums1.length;
+             int n2 = nums2.length;
 
-           if(m <= n)
-           {
-             smallArray = nums1;
-             largeArray = nums2;
-           }
-           else
-           {
-             smallArray = nums2;
-             largeArray = nums1;
-           }
+             int low = 0;
+             int high = n1;
 
+             while(low <= high)
+             {
+                int mid1 = low + (high - low)/2;
+                int mid2 = (n1 + n2)/2 - mid1;
 
-           int low = 0;
-           int high = smallArray.length;
+                int l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1-1];
+                int l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2-1];
 
-           int leftHalf = (m+n+1)/2;
+                int r1 = (mid1 >= n1) ? Integer.MAX_VALUE : nums1[mid1];
+                int r2 = (mid2 >= n2) ? Integer.MAX_VALUE : nums2[mid2];
 
+                if(l1 <= r2  && l2 <= r1)
+                   return ((n1 + n2) % 2 == 0) ? (Math.max(l1,l2) + Math.min(r1,r2))/2.0 : Math.min(r1,r2) + 0.0;
+                else if(l1 > r2)
+                   high = mid1 - 1;
+                else
+                   low = mid1 + 1;
+             }
 
-           while(low <= high)
-           {
-               int mid1 = low + (high - low)/2;
-               int mid2 = leftHalf - mid1;
-
-               int l1 = Integer.MIN_VALUE;
-               int l2 = Integer.MIN_VALUE;
-
-               int r1 = Integer.MAX_VALUE;
-               int r2 = Integer.MAX_VALUE;
-
-               if(mid1 > 0)
-               l1 = smallArray[mid1-1];
-
-               if(mid2 > 0)
-               l2 = largeArray[mid2-1];
-
-               if(mid1 < smallArray.length)
-               r1 = smallArray[mid1];
-
-               if(mid2 < largeArray.length)
-               r2 = largeArray[mid2];
+              
+           return -1;   // dummy return value
 
 
-               if(l1 <= r2 && l2 <= r1)
-               {
-
-                   if((m+n)%2 == 0)
-                   {
-                    return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
-                   }
-
-                   return Math.max(l1,l2);
-               }
-               else if(l1 > r2)
-               {
-                  high = mid1 - 1;
-               }
-               else
-               {
-                 low = mid1 + 1;
-               }
-           }
-
-           return -1; // dummy return value;
     }
 }
